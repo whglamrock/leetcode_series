41c79d1b521a63b7a0610ad744341f814966cc4a
whglamrock <haowu19930908@gmail.com>
2017-03-28 09:36:55 -0400

    Add new files

--- leetcode_series/leetcode246 Strobogrammatic.py
+++ leetcode_series/leetcode246 Strobogrammatic.py
@@ -2,12 +2,12 @@
 class Solution(object):
     def isStrobogrammatic(self, num):
 
-        for i in xrange(len(num)/2+1):
-            if num[i] == num[len(num)-i-1]:
+        for i in xrange(len(num) / 2 + 1):
+            if num[i] == num[len(num) - i - 1]:
                 if num[i] != '0' and num[i] != '8' and num[i] != '1':
                     return False
             else:
-                if {num[i],num[len(num)-i-1]} != {'6','9'}:
+                if {num[i], num[len(num)- i - 1]} != {'6', '9'}:
                     return False
 
         return True
