47408ff2ccaa430a8ea20bca98e9f21c2b0e9115
whglamrock <haowu19930908@gmail.com>
2017-03-29 11:08:13 -0400

    Add new files

--- leetcode_series/fkuagain.py
+++ leetcode_series/fkuagain.py
@@ -1,36 +1,11 @@
+from collections import Counter
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        if not nums:
-            return []
+a = [1, 2, 3, 4, 5]
+b = Counter(a)
+print b
 
-        dic = {}
-        for i, num in enumerate(nums):
-            print i, num
-            if num not in dic:
-                dic[num] = [i]
-            else:
-                dic[num].append(i)
-
-        for num in nums:
-            if target - num in dic:
-                if target - num != num:
-                    return [dic[num][0], dic[target - num][0]]
-                else:
-                    if len(dic[num]) > 1:
-                        return dic[num][:2]
-                    else:
-                        continue
-
-        return []
-
-
-
-nums = [3, 5, 4, 5, 2, 3, 1, 7]
-target = 10
-Sol = Solution()
-print Sol.twoSum(nums, target)
-
+for key in b.keys():
+   print key
 
 
 
