8cd7f8af6f16897ad5da646e8b2542ff30e5775d
whglamrock <haowu19930908@gmail.com>
2017-04-13 16:47:58 -0400

    Add changes

--- leetcode_series/leetcode282 Expression Add Operators.py
+++ leetcode_series/leetcode282 Expression Add Operators.py
@@ -8,32 +8,33 @@
 #   path with the same key could yield different final results because of the variation the last claus
 # Plus, there is no solution with memoization brought up in discuss
 
+
 class Solution(object):
     def addOperators(self, num, target):
 
-        if not num: return []
-
         self.ans = []
-        self.target = target
         for i in xrange(1, len(num) + 1):
+            # it is legal when num[:i] == '0', and the "i == 1" condition is for this
             if i == 1 or (i > 1 and num[0] != '0'):
-                self.dfs(num[i:], num[:i], int(num[:i]), int(num[:i]))
+                self.helper(num[i:], int(num[:i]), num[:i], int(num[:i]), target)
 
         return self.ans
 
-    # the last means the last claus, which is very import if the next operator is '*'
-    def dfs(self, num, leftexpression, leftresult, last):
+    # lastclause is set for operator '*'
+    def helper(self, num, leftresult, leftexpression, lastclause, target):
 
         if not num:
-            if leftresult == self.target:
+            if leftresult == target:
                 self.ans.append(leftexpression)
             return
 
+        # because the nextnum is num[:i], i starts from 1
         for i in xrange(1, len(num) + 1):
-            # we need to avoid the situation when the val is '0XXX...'
+            val = num[:i]
+            nextnum = num[i:]
+            # we need to put the "whether starts with 0" check here instead of arbitrarily using
+            #   "if num[0] == '0': return " to end the recursion
             if i == 1 or (i > 1 and num[0] != '0'):
-                val = num[:i]
-                self.dfs(num[i:], leftexpression + '+' + val, leftresult + int(val), int(val))
-                # make the parameter last '-int(val)' in '-' case, so the parameter last in '*' can consistent
-                self.dfs(num[i:], leftexpression + '-' + val, leftresult - int(val), -int(val))
-                self.dfs(num[i:], leftexpression + '*' + val, leftresult - last + last * int(val), last * int(val))
+                self.helper(nextnum, leftresult + int(val), leftexpression + '+' + val, int(val), target)
+                self.helper(nextnum, leftresult - int(val), leftexpression + '-' + val, -int(val), target)
+                self.helper(nextnum, leftresult - lastclause + lastclause * int(val), leftexpression + '*' + val, lastclause * int(val), target)
