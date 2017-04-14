3c9b716c129b627ae860b8929d129680dd30a0fc
whglamrock <haowu19930908@gmail.com>
2017-04-14 16:38:00 -0400

    Add changes

--- leetcode_series/leetcode257 Binary Tree Paths.py
+++ leetcode_series/leetcode257 Binary Tree Paths.py
@@ -9,22 +9,24 @@ class TreeNode:
         self.right = None
 
 
-class Solution:
+class Solution(object):
     def binaryTreePaths(self, root):
 
         if not root:
             return []
-        res = []
-        self.dfs(root, "", res)
-        return res
 
-    def dfs(self, root, ls, res):
-        if not root.left and not root.right:
-            res.append(ls+str(root.val))
-        if root.left:
-            self.dfs(root.left, ls+str(root.val)+"->", res)
-        if root.right:
-            self.dfs(root.right, ls+str(root.val)+"->", res)
+        self.ans = []
+        def helper(node, path):
+            if not node.left and not node.right:
+                self.ans.append(path)
+                return
+            if node.left:
+                helper(node.left, path + '->' + str(node.left.val))
+            if node.right:
+                helper(node.right, path + '->' + str(node.right.val))
+
+        helper(root, str(root.val))
+        return self.ans
 
 
 
