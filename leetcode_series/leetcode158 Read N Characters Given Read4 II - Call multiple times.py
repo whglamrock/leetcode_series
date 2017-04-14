4013cc26a0a924de9a258d2e82ee6dcba0d06ece
whglamrock <haowu19930908@gmail.com>
2017-04-14 15:45:13 -0400

    Add changes

--- leetcode_series/leetcode158 Read N Characters Given Read4 II - Call multiple times.py
+++ leetcode_series/leetcode158 Read N Characters Given Read4 II - Call multiple times.py
@@ -20,6 +20,8 @@ class Solution(object):
 
             cnm = [''] * 4
             l = read4(cnm)
+            # if there less than four chars read, e.g., three, the
+            #   cnm will be like ['a', 'b', 'c'] instead of ['a', 'b', 'c', '']
             self.queue.extend(cnm)
 
             curlen = min(len(self.queue), n - index)
@@ -30,3 +32,41 @@ class Solution(object):
             if curlen == 0: break
 
         return index
+    
+
+
+
+'''
+from collections import deque
+
+class Solution(object):
+    def __init__(self):
+
+        self.q = deque()
+
+    def read(self, buf, n):
+
+        i = 0
+        if self.q:
+            while n and self.q:
+                buf[i] = self.q.popleft()
+                n -= 1
+                i += 1
+
+        while True:
+            cnm = [''] * 4
+            l = read4(cnm)
+            for j in xrange(l):
+                # actually this condition could be saved because there won't be empty chars in cnm
+                if cnm[j]:
+                    self.q.append(cnm[j])
+            if len(self.q) >= n or l == 0:
+                break
+
+        while n and self.q:
+            buf[i] = self.q.popleft()
+            i += 1
+            n -= 1
+
+        return i
+'''
