84e0174f0989c1216cbb37ffe0636b5b5f4f6189
whglamrock <haowu19930908@gmail.com>
2017-04-24 21:11:23 -0400

    add changes

--- leetcode_series/leetcode218 The Skyline Problem.py
+++ leetcode_series/leetcode218 The Skyline Problem.py
@@ -2,29 +2,42 @@
 # explanation for the skyline problem: https://briangordon.github.io/2014/08/the-skyline-problem.html
 # code from: https://discuss.leetcode.com/topic/26420/14-line-python-code-straightforward-easy-to-understand
 
-import heapq
+from heapq import *
+
 class Solution(object):
     def getSkyline(self, buildings):
 
-        sky = [[-1, 0]]
-        def addsky(pos, hi):
-            if sky[-1][-1] != hi:
-                sky.append([pos, hi])
+        if not buildings:
+            return []
 
-        position = set(b[0] for b in buildings) | set(b[1] for b in buildings)
-        i = 0
+        # because we only add the x position to positions,
+        #   we only one of multiple Lis with the same value
+        positions = set()
+        for item in buildings:
+            positions.add(item[0])
+            positions.add(item[1])
+
+        # while the vertical line "scans" the sky, the live stores the current "live" positions:
+        #   a building is "live" is the current line crosses the building
         live = []
+        # it could any [x, 0]. the second element has to be 0 because of the following if statement
+        sky = [[-1, 0]]
 
-        for t in sorted(position):
+        i = 0
+        # sorted function changes the set into a sorted list
+        for t in sorted(positions):
+            # add new buildings that are alive now
             while i < len(buildings) and buildings[i][0] <= t:
-                heapq.heappush(live, [-buildings[i][2], buildings[i][1]])
+                heappush(live, [-buildings[i][2], buildings[i][1]])
                 i += 1
-
+            # discard the buildings that have been passed by the scan line
             while live and live[0][1] <= t:
-                heapq.heappop(live)
-
+                heappop(live)
+            # if we have live buildings, then h != 0
             h = -live[0][0] if live else 0
-            addsky(t, h)
+            # according to the rules of "key points"
+            if sky[-1][-1] != h:
+                sky.append([t, h])
 
         return sky[1:]
 
