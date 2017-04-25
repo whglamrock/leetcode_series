7ec8cf811d5648f39e1c865506470e411e0e1c76
whglamrock <haowu19930908@gmail.com>
2017-04-25 16:58:11 -0400

    add changes

--- leetcode_series/leetcode49 Group Anagrams.py
+++ leetcode_series/leetcode49 Group Anagrams.py
@@ -1,26 +1,29 @@
 
+# idea: use prime numbers to build the key
+# Two strings with different lengths can't have the same key: if that happens, then exists m and n (m != n)
+#   where the product of m prime numbers == the product of n prime numbers, which is impossible.
+#   So the prime number approach can keep each key unique
+
+from collections import defaultdict
+
 class Solution(object):
     def groupAnagrams(self, strs):
 
-        dick = {}
-        for i in xrange(len(strs)):
-            newdick = []
-            for char in strs[i]:
-                newdick.append(char)
-            newdick.sort()
-            newitem = ''.join(newdick)
-            if newitem not in dick:
-                dick[newitem] = [i]
-            else:
-                dick[newitem].append(i)
+        primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+        def calculatekey(string):
 
-        ans = []
-        for item in dick:
-            newans = []
-            for index in dick[item]:
-                newans.append(strs[index])
-            ans.append(newans)
+            key = 1
+            for letter in string:
+                key *= primes[ord(letter) - ord('a')]
 
+            return key
+
+        dic = defaultdict(list)
+        for string in strs:
+            key = calculatekey(string)
+            dic[key].append(string)
+
+        ans = [value for value in dic.values()]
         return ans
 
 
