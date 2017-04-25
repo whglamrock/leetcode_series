814d4c59d00901d492a6bb87f9045944697f4363
whglamrock <haowu19930908@gmail.com>
2017-04-25 17:52:07 -0400

    add changes

--- leetcode_series/leetcode238 Product of Array Except Self.py
+++ leetcode_series/leetcode238 Product of Array Except Self.py
@@ -5,13 +5,18 @@
 class Solution(object):
     def productExceptSelf(self, nums):
 
+        # there is no need to check whether nums is empty because n > 1
         n = len(nums)
-        res = [0] * len(nums)
+        res = [0] * n
         res[0] = 1
+
+        # in first pass, make each element of res the product of its left elements (excluding self)
         for i in xrange(1, n):
             res[i] = res[i - 1] * nums[i - 1]
+        # e.g., the nums = [1, 2, 3, 4], then res = [1, 1, 2, 6]
 
         right = 1
+        # the second scan (also excluding self) has to start from the last element
         for i in xrange(n - 1, -1, -1):
             res[i] *= right
             right *= nums[i]
