19b97baa05dda9a9ec254968914d531f2d20e209
whglamrock <haowu19930908@gmail.com>
2017-04-25 17:33:49 -0400

    add changes

--- leetcode_series/leetcode79 Word Search.py
+++ leetcode_series/leetcode79 Word Search.py
@@ -1,33 +1,37 @@
 
-# recursive solution. Sometimes it's hard to find iterative solution for dfs.
+# recursive solution. Sometimes it's hard to find iterative solution for this dfs.
 
 class Solution(object):
     def exist(self, board, word):
 
-        if not board:
+        if not word or not board or not board[0]:
             return False
-        for i in xrange(len(board)):
-            for j in xrange(len(board[0])):
-                if self.dfs(board, i, j, word):
+
+        self.board = board
+        m, n = len(board), len(board[0])
+
+        def dfs(i, j, word, k):
+
+            if k == len(word):
+                return True
+            if i < 0 or i >= m or j < 0 or j >= n:  # out of range
+                return False
+            if self.board[i][j] != word[k]:
+                return False
+
+            tmp = self.board[i][j]
+            self.board[i][j] = 0    # make the element a number, avoid visit again
+            res = dfs(i - 1, j, word, k + 1) or dfs(i + 1, j, word, k + 1) or \
+                  dfs(i, j - 1, word, k + 1) or dfs(i, j + 1, word, k + 1)
+            self.board[i][j] = tmp
+
+            return res
+
+        for i in xrange(m):
+            for j in xrange(n):
+                if dfs(i, j, word, 0):
                     return True
         return False
-
-    # check whether can find word, start at (i,j) position
-    def dfs(self, board, i, j, word):
-
-        if len(word) == 0: # all the characters are checked
-            return True
-        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or word[0] != board[i][j]:
-            return False
-
-        tmp = board[i][j]  # first character is found, check the remaining part
-        board[i][j] = "#"  # avoid visit again
-        # check whether can find "word" along one direction
-        res = self.dfs(board, i+1, j, word[1:]) or self.dfs(board, i-1, j, word[1:]) \
-        or self.dfs(board, i, j+1, word[1:]) or self.dfs(board, i, j-1, word[1:])   # P.S. the use of '\'
-        board[i][j] = tmp
-
-        return res
 
 
 
