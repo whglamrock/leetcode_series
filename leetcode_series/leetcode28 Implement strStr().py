fc77282b26f3355cfdc39b9efe69fbce59a89efb
whglamrock <haowu19930908@gmail.com>
2017-05-22 14:06:15 -0400

    add chnages

--- leetcode_series/leetcode28 Implement strStr().py
+++ leetcode_series/leetcode28 Implement strStr().py
@@ -3,49 +3,60 @@
 # Theory from youtube: https://www.youtube.com/watch?v=GTJr8OvyEVQ
 
 class Solution(object):
-    def ComputePrefixFunction(self, needle):
+    def prefixComputation(self, needle):
 
-        pat = [0] * len(needle)
-        # j is the prefix pointer, i is the suffix pointer
+        n = len(needle)
+        pat = [0] * n
+        # j is the prefix pointer, and i is the suffix pointer
         j, i = 0, 1
 
-        while i < len(needle):
+        while i < n:
             if needle[i] == needle[j]:
                 pat[i] = j + 1
                 i += 1
                 j += 1
-            elif j == 0:    # it's actually j = pat[j - 1] for the case when j == 0
+            elif j == 0: # special case of "j = pat[j - 1]" for j == 0
                 pat[i] = 0
                 i += 1
-            else:   # at this point, needle[j] != needle[i]
-                j = pat[j - 1]  # we need to the prefix position to compare in next loop
+            else:
+                # don't do i += 1 or pat[i] = pat[j - 1] here.
+                # we need to change j:
+                #   1) if needle[i] == needle[pat[j - 1]], pat[i] will = pat[j - 1] + 1, not pat[j - 1];
+                #   2) if needle[i] != needle[pat[j - 1]], the "pat[i] = pat[j - 1]" will make loop infinite
+                #      because i, j don't += 1 anymore.
+                j = pat[j - 1]
 
         #print pat
         return pat
 
+    # return the index of matching point
     def strStr(self, haystack, needle):
 
-        if (not needle) or len(needle) == 0:
+        if not needle:
             return 0
-        if (not haystack) or len(haystack) < len(needle):
+        if not haystack or len(haystack) < len(needle):
             return -1
 
-        n = len(haystack)
-        m = len(needle)
-        # at this time, i is the haystack pointer, j is the needle pointer
+        n, m = len(haystack), len(needle)
         i, j = 0, 0
-        pat = self.ComputePrefixFunction(needle)
+        pat = self.prefixComputation(needle)
 
         while i < n and j < m:
             if haystack[i] == needle[j]:
                 i += 1
                 j += 1
-                if j == len(needle):
+                if j == m:
                     return i - j
             elif j == 0:
                 i += 1
             else:
                 j = pat[j - 1]
+                # we don't do i += 1 here, because needle[j] don't match haystack[i]
+                # 1) needle[:j] matches haystack[i - j:i]
+                # 2) after doing the switch on j, j becomes pat[j - 1]
+                # 3) so needle[:pat[j - 1]] matches haystack[i - j:i]
+                # 4) we still don't know if haystack[i] matches needle[:pat[j - 1] + 1]
+                # 5) thus, we needa check haystack[i] and don't do i += 1
 
         return -1
 
