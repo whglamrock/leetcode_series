2588ca733fa6547fc7ceca001c6106e2c6e569ec
whglamrock <haowu19930908@gmail.com>
2017-06-12 17:50:22 -0400

    add changes

--- leetcode_series/leetcode373 Find K Pairs with Smallest Sums.py
+++ leetcode_series/leetcode373 Find K Pairs with Smallest Sums.py
@@ -1,24 +1,29 @@
 
-# brilliant idea from Stefan: https://discuss.leetcode.com/topic/50450/slow-1-liner-to-fast-solutions
+from heapq import *
 
-import heapq
+# the idea is pretty much like the lc378. consider the nums1, nums2 are the rows and columns of
+#   a 2-D array
 
 class Solution(object):
     def kSmallestPairs(self, nums1, nums2, k):
 
-        queue = []
-        def push(i, j):
-            if i < len(nums1) and j < len(nums2):
-                heapq.heappush(queue, [nums1[i] + nums2[j], i, j])
+        if not nums1 or not nums2 or not k:
+            return []
 
-        push(0, 0)
-        pairs = []
-        while queue and len(pairs) < k:
-            _, i, j = heapq.heappop(queue)
-            pairs.append([nums1[i], nums2[j]])
-            push(i, j + 1)
-            if j == 0:
-                push(i + 1, 0)
-            #print queue
+        n1, n2 = len(nums1), len(nums2)
+        q = []
+        for i2 in xrange(n2):
+            heappush(q, [nums1[0] + nums2[i2], (0, i2)])
 
-        return pairs
+        ans = []
+        while k:
+            if not q:
+                break
+            currsum, (i1, i2) = heappop(q)
+            ans.append([nums1[i1], nums2[i2]])
+            k -= 1
+            if i1 == n1 - 1:
+                continue
+            heappush(q, [nums1[i1 + 1] + nums2[i2], (i1 + 1, i2)])
+
+        return ans
