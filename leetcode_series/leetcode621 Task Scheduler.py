50f3e0117030fc879ef4bb72fcf69f0a291ba412
whglamrock <haowu19930908@gmail.com>
2017-06-21 15:41:54 -0400

    add changes

---
+++ leetcode_series/leetcode621 Task Scheduler.py
@@ -0,0 +1,23 @@
+
+# see my explanation: https://discuss.leetcode.com/topic/92852/concise-java-solution-o-n-time-o-26-space/28
+
+class Solution(object):
+    def leastInterval(self, tasks, n):
+
+        c = [0] * 26
+        for task in tasks:
+            c[ord(task) - ord('A')] += 1
+        c.sort()
+
+        i = 25
+        while i >= 0 and c[i] == c[25]:
+            i -= 1
+
+        return max(len(tasks), (c[25] - 1) * (n + 1) + 25 - i)
+
+
+
+Sol = Solution()
+tasks = ['A', 'B', 'A', 'C', 'A', 'E', 'A', 'D', 'B']
+n = 4
+print Sol.leastInterval(tasks, n)
