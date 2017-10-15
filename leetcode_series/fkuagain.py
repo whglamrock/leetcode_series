0cb88c0c8148b68303f7f34776075b299020070a
whglamrock <haowu19930908@gmail.com>
2017-10-14 17:55:38 -0700

    add changes

--- leetcode_series/fkuagain.py
+++ leetcode_series/fkuagain.py
@@ -1,4 +1,14 @@
-print 27 ** -1 ** 3
+
+s = "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga"
+l, r = 0, len(s) - 1
+while l < r:
+    if s[l] != s[r]:
+        print str(l) + ':', s[l], str(r) + ':', s[r]
+        break
+    l += 1
+    r -= 1
+
+
 
 
 
---
+++ leetcode_series/leetcode680 Valid Palindrome II.py
@@ -0,0 +1,25 @@
+
+class Solution(object):
+    def validPalindrome(self, s):
+
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return self.isPalindrome(s, l + 1, r) or self.isPalindrome(s, l, r - 1)
+            l += 1
+            r -= 1
+
+        return True
+
+    # need to consider 2 possibilities when (s[l - 1] == s[r] and s[l + 1] == s[r]):
+    #   l += 1 or r -=1 could both work
+    def isPalindrome(self, s, l, r):
+
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
