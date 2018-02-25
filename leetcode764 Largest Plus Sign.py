4ea1719d8e419339dc510149eca928d978387788
Hao Wu <how@amazon.com>
2018-02-24 17:07:56 -0800

    add changes

--- leetcode764 Largest Plus Sign.py
+++ leetcode764 Largest Plus Sign.py
@@ -2,39 +2,46 @@
 class Solution(object):
     def orderOfLargestPlusSign(self, N, mines):
 
+        # at first, set each position value to N instead of 1,
+        #   because we need to get the min(grid[i][j], leg length)
         grid = [[N for j in xrange(N)] for i in xrange(N)]
         for mine in mines:
-            grid[mine[0]][mine[1]] = 0
+            x, y = mine[0], mine[1]
+            grid[x][y] = 0
 
-        # longest stretch in left direction
+        # legs are on the left:
         for i in xrange(N):
-            l = 0
+            r = 0
             for j in xrange(N):
-                l = l + 1 if grid[i][j] != 0 else 0
-                grid[i][j] = min(grid[i][j], l)
-        # longest stretch in right direction
+                r = r + 1 if grid[i][j] != 0 else 0
+                grid[i][j] = min(grid[i][j], r)
+
+        # legs are on the right:
         for i in xrange(N):
             r = 0
             for j in xrange(N - 1, -1, -1):
                 r = r + 1 if grid[i][j] != 0 else 0
                 grid[i][j] = min(grid[i][j], r)
-        # longest stretch in up direction
+
+        # legs are above:
         for j in xrange(N):
-            u = 0
+            r = 0
             for i in xrange(N):
-                u = u + 1 if grid[i][j] != 0 else 0
-                grid[i][j] = min(grid[i][j], u)
-        # longest stretch in down direction
+                r = r + 1 if grid[i][j] != 0 else 0
+                grid[i][j] = min(grid[i][j], r)
+
+        # legs are under:
         for j in xrange(N):
-            d = 0
+            r = 0
             for i in xrange(N - 1, -1, -1):
-                d = d + 1 if grid[i][j] != 0 else 0
-                grid[i][j] = min(grid[i][j], d)
+                r = r + 1 if grid[i][j] != 0 else 0
+                grid[i][j] = min(grid[i][j], r)
 
         ans = 0
         for i in xrange(N):
             for j in xrange(N):
                 ans = max(ans, grid[i][j])
+
         return ans
 
 
