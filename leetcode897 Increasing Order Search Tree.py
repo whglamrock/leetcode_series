0976ab4b70f67f22b97a2f8d22e71e5dafbad377
Hao Wu <how@amazon.com>
2018-09-18 00:03:56 -0700

    add changes

--- leetcode897 Increasing Order Search Tree.py
+++ leetcode897 Increasing Order Search Tree.py
@@ -8,6 +8,31 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
+
+class Solution(object):
+
+    def increasingBST(self, root):
+
+        # in order traversal
+        def inOrder(root, traversal):
+            if root.left:
+                inOrder(root.left, traversal)
+            traversal.append(root)
+            if root.right:
+                inOrder(root.right, traversal)
+            return traversal
+
+        traversal = inOrder(root, [])
+        for i in xrange(len(traversal) - 1):
+            traversal[i].left = None
+            traversal[i].right = traversal[i + 1]
+        traversal[-1].left, traversal[-1].right = None, None
+
+        return traversal[0]
+
+
+
+'''
 # P.S., the problem description didn't tell it is a BST...lol
 class Solution(object):
 
@@ -31,3 +56,4 @@ class Solution(object):
         traversal[-1].left, traversal[-1].right = None, None
 
         return traversal[0]
+'''
