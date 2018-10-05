4c23d5901e82c6d5fde0f9e26aaef0237999f606
Hao Wu <how@amazon.com>
2018-10-05 10:32:06 -0700

    add changes

--- leetcode200 Number of Islands.py
+++ leetcode200 Number of Islands.py
@@ -6,6 +6,45 @@ then visiting other grid[i][j]s, no duplicate '1's will be counted.
 O(N^2) time.
 '''
 
+class Solution(object):
+    def numIslands(self, grid):
+
+        if not grid or not grid[0]:
+            return 0
+
+        def bfs(i, j, m, n, grid):
+            if i - 1 >= 0 and grid[i - 1][j] == '1':
+                grid[i - 1][j] = '0'
+                bfs(i - 1, j, m, n, grid)
+            if i + 1 < m and grid[i + 1][j] == '1':
+                grid[i + 1][j] = '0'
+                bfs(i + 1, j, m, n, grid)
+            if j - 1 >= 0 and grid[i][j - 1] == '1':
+                grid[i][j - 1] = '0'
+                bfs(i, j - 1, m, n, grid)
+            if j + 1 < n and grid[i][j + 1] == '1':
+                grid[i][j + 1] = '0'
+                bfs(i, j + 1, m, n, grid)
+
+        m, n = len(grid), len(grid[0])
+        count = 0
+        for i in xrange(m):
+            for j in xrange(n):
+                if grid[i][j] == '1':
+                    bfs(i, j, m, n, grid)
+                    count += 1
+
+        return count
+
+
+
+grid = [['1','1','1','1','0'],['1','1','0','1','0'],['1','1','0','0','0'],['0','0','0','0','0']]
+Sol = Solution()
+print Sol.numIslands(grid)
+
+
+
+'''
 from collections import deque
 
 class Solution(object):
@@ -41,11 +80,4 @@ class Solution(object):
                              grid[x][y + 1] = '0'
 
         return numofisland
-
-
-
-grid = [['1','1','1','1','0'],['1','1','0','1','0'],['1','1','0','0','0'],['0','0','0','0','0']]
-Sol = Solution()
-print Sol.numIslands(grid)
-
-
+'''
