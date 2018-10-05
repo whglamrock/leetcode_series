3744b8a7790e5ab4edd6145dd9544b3bb2ee190d
Hao Wu <how@amazon.com>
2018-10-05 11:30:20 -0700

    add changes

--- leetcode127 Word ladder.py
+++ leetcode127 Word ladder.py
@@ -1,40 +1,38 @@
 
-# the stupid leetcode changed the type of wordList from set to list.
+# note the problem definition and all the conditions
 
 class Solution(object):
     def ladderLength(self, beginWord, endWord, wordList):
 
         if not wordList:
-            return 0    # according to the problem definition
+            return 0  # according to the problem definition
         # it's told by the definition that beginWord != endWord
 
         wordList = set(wordList)
-        # to record the current status of transformation
+        letters = "abcdefghijklmnopqrstuvwxyz"
         todo = {beginWord}
-        # we can discard the beginWord or not
-        #wordList.discard(beginWord)
-        length = 2
 
+        length = 2
         while todo:
-            new = set()
+            next = set()
             for word in todo:
                 for i in xrange(len(word)):
-                    for char in 'abcdefghijklmnopqrstuvwxyz':
-                        newword = word[:i] + char + word[i + 1:]
-                        if newword in wordList:
-                            wordList.discard(newword)
-                            new.add(newword)
-            if endWord in new:
-                return length
+                    for char in letters:
+                        newWord = word[:i] + char + word[i + 1:]
+                        if newWord in wordList:
+                            if newWord == endWord:
+                                return length
+                            next.add(newWord)
+                            wordList.discard(newWord)  # this word is visited
+            todo = next
             length += 1
-            todo = new
 
-        return 0    # the above BFS already tried out all possibilities
+        return 0 # the above BFS already tried out all possibilities
 
 
 
 beginWord = "hit"
-endWord = "hit"
-wordList = {"hit", "hot","dot","dog","lot","log"}
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log","cog"]
 Sol = Solution()
 print Sol.ladderLength(beginWord, endWord, wordList)
