64118159c5de7d86569411c3610b1caf639c1521
Hao Wu <how@amazon.com>
2018-10-28 17:22:21 -0700

    add changes

--- leetcode692 Top K Frequent Words.py
+++ leetcode692 Top K Frequent Words.py
@@ -1,6 +1,6 @@
 
+from collections import Counter
 from heapq import *
-from collections import Counter, defaultdict
 
 class Node:
     def __init__(self, count, word):
@@ -14,33 +14,24 @@ class Node:
             return self.word < o.word
 
 
-# O(nlogk) solution
-
 class Solution(object):
     def topKFrequent(self, words, k):
 
-        q = []
         wordCount = Counter(words)
+        q = []
         for word in wordCount:
             count = wordCount[word]
             node = Node(count, word)
             heappush(q, node)
-            if len(q) > k:
+            while len(q) > k:
                 heappop(q)
 
-        intermediate = defaultdict(list)
+        ans = []
         while q:
-            # will always pop the smallest count and biggest word
             node = heappop(q)
-            count, word = node.count, node.word
-            intermediate[count].append(word)
+            ans.append(node.word)
 
-        res = []
-        for count in sorted(intermediate.keys())[::-1]:
-            # the word is added in reverse alphabetic order when count is same
-            res += intermediate[count][::-1]
-
-        return res
+        return ans[::-1]
 
 
 
