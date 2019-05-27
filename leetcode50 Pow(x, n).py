0d3834f73dbea7489fc8b71d25f56febdd92ddb3
Hao Wu <how@amazon.com>
2019-05-27 16:44:19 -0700

    add changes

--- leetcode50 Pow(x, n).py
+++ leetcode50 Pow(x, n).py
@@ -1,19 +1,30 @@
 
-# O(logN) running time solution
-
 class Solution(object):
     def myPow(self, x, n):
-
-        if n == 0: return 1
-
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
         if n < 0:
-            n = -n
-            x = 1 / x
-
-        if n % 2 == 0:
-            return self.myPow(x * x, n / 2)
+            return 1.0 / self.fastPow(x, -n)
         else:
-            return x * self.myPow(x * x, n / 2)
+            return self.fastPow(x, n)
+
+    # use recursion to do the binary scale down job
+    # we assume n is positive right here
+    def fastPow(self, x, n):
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+
+        half = self.fastPow(x, n / 2)
+        if n % 2 == 0:
+            return half * half
+        else:
+            # note that we still consider this extra 'x *' step as idempotent, so overall time complexity is logN
+            return x * half * half
 
 
 
