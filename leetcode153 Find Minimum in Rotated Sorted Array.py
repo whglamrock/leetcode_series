3d00deb822fcaa6dc510d27ae54107b6b30e2ac8
Hao Wu <how@amazon.com>
2019-05-27 18:43:22 -0700

    add changes

--- leetcode153 Find Minimum in Rotated Sorted Array.py
+++ leetcode153 Find Minimum in Rotated Sorted Array.py
@@ -1,17 +1,28 @@
 
-'''
-binary search
-'''
-
 class Solution(object):
     def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return -1
 
-        i = 0
-        j = len(nums) - 1
-        while i < j:
-            m = (i+j) / 2
-            if nums[m] > nums[j]:
-                i = m + 1
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) / 2
+            if nums[l] <= nums[m] <= nums[r]:
+                return nums[l]
+            # nums[l] == nums[m] is when search range only contains 1 or 2 nums
+            elif nums[l] <= nums[m]:
+                l = m + 1
+            # in this case, m is still candidate index
             else:
-                j = m
-        return nums[j]  # 'return nums[i]' is the same, according to the while loop statement
+                # consider when in next loop the search range becomes 1, the first if condition will satisfy
+                r = m
+
+
+
+Sol = Solution()
+nums = [4,5,6,7,0,1,2]
+print Sol.findMin(nums)
