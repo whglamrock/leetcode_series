2845252ecca50928d6afc3705e74f20b677ef336
Hao Wu <how@amazon.com>
2019-06-30 17:49:09 -0700

    add changes

---
+++ leetcode908 Smallest Range I.py
@@ -0,0 +1,11 @@
+
+# once you understand the question it's easy asf
+
+class Solution(object):
+    def smallestRangeI(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        return max(max(A) - min(A) - 2 * K, 0)
