1c2df71ac9307a55d93dca715fdc1ff633137857
Hao Wu <how@amazon.com>
2019-07-04 17:50:52 -0700

    add changes

--- leetcode415 Add Strings.py
+++ leetcode415 Add Strings.py
@@ -1,29 +1,20 @@
 
-from collections import deque
+# the question says "You must not use any built-in BigInteger library or convert the inputs to integer directly"
+  # so we need to use the ord() function which gets the ascii number
 
 class Solution(object):
     def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        int1 = 0
+        for char in num1:
+            int1 = int1 * 10 + ord(char) - ord('0')
 
-        res = deque()
-        trans = 0
-        i = 0
-        if len(num1) > len(num2):
-            num1, num2 = num2, num1
+        int2 = 0
+        for char in num2:
+            int2 = int2 * 10 + ord(char) - ord('0')
 
-        num1 = num1[::-1]
-        num2 = num2[::-1]
-        n = len(num1)
-        while i < n:
-            digit = (int(num1[i]) + int(num2[i]) + trans) % 10
-            res.appendleft(str(digit))
-            trans = (int(num1[i]) + int(num2[i]) + trans) / 10
-            i += 1
-
-        while n < len(num2):
-            digit = (int(num2[n]) + trans) % 10
-            res.appendleft(str(digit))
-            trans = (int(num2[n]) + trans) / 10
-            n += 1
-
-        if trans: res.appendleft(str(trans))
-        return ''.join(res)
+        return str(int1 + int2)
