a6981b8d4d913262abef5e700a2bf16b21d0faf9
Hao Wu <how@amazon.com>
2019-07-13 18:04:01 -0700

    add changes

--- leetcode5 Longest Palindromic Substring.py
+++ leetcode5 Longest Palindromic Substring.py
@@ -1,30 +1,45 @@
 
 """
-This O(n^2) algorithm is based on: every time the substring grows, it can only grow by ONE
-charater or TWO. The proof can be found on: https://leetcode.com/discuss/21332/python-o-n-2-method-with-some-optimization-88ms
+O(n^2) time & space. palindrome can only grow from palindromes. Ultimately the most basic palindrome forms from
+1 char or r chars
 """
 
 class Solution(object):
     def longestPalindrome(self, s):
-
-        if (not s) or len(s) == 0:
-            return s
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ''
 
         n = len(s)
-        maxlen = 1
-        start, end = 0, 0
-        i = 1
-        while i < n:
-            if i - maxlen > 0 and s[i - maxlen - 1: i + 1] == s[i - maxlen - 1: i + 1][::-1]:
-                start, end = i - maxlen - 1, i
-                maxlen += 2
-            elif i - maxlen >= 0 and s[i - maxlen: i + 1] == s[i - maxlen: i + 1][::-1]:
-                start, end = i - maxlen, i
-                maxlen += 1
-            #print i, maxlen
-            i += 1
+        # dp[i][j] means whether s[i: j + 1] is a palindrome
+        dp = [[0 for j in xrange(n)] for i in xrange(n)]
+        for i in xrange(n):
+            dp[i][i] = 1
 
-        return s[start: end + 1]
+        # deal with situation like 'aa', 'bb'
+        for i in xrange(1, n):
+            if s[i] == s[i - 1]:
+                dp[i - 1][i] = 1
+
+        # then all palingdromes can grow from 1 char or 2 chars
+        # the new palinfromes found from this point will be at least 3 chars long
+        for i in xrange(2, n):
+            for j in xrange(1, i):
+                if dp[j][i - 1] == 1 and s[j - 1] == s[i]:
+                    dp[j - 1][i] = 1
+
+        maxLen = 0
+        ans = [0, 0]
+        for i in xrange(n):
+            for j in xrange(i, n):
+                if dp[i][j] == 1 and j - i > maxLen:
+                    ans = [i, j]
+                    maxLen = j - i
+
+        return s[ans[0]: ans[1] + 1]
 
 
 
