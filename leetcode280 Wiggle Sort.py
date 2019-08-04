8c4fe98be13081153187a8562c8b72f38edc110e
Hao Wu <how@amazon.com>
2019-08-04 12:58:23 -0700

    add changes

--- leetcode280 Wiggle Sort.py
+++ leetcode280 Wiggle Sort.py
@@ -1,29 +1,33 @@
 
 class Solution(object):
     def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return
 
         i = 0
         while i < len(nums) - 1:
             if i % 2 == 0:
-                if nums[i + 1] < nums[i]:
-                    temp = nums[i + 1]
-                    nums[i + 1] = nums[i]
-                    nums[i] = temp
+                if nums[i] > nums[i + 1]:
+                    self.exchange(nums, i, i + 1)
             else:
-                if nums[i + 1] > nums[i]:
-                    temp = nums[i + 1]
-                    nums[i + 1] = nums[i]
-                    nums[i] = temp
+                if nums[i] < nums[i + 1]:
+                    self.exchange(nums, i, i + 1)
             i += 1
 
-        #return nums     # the leetcode asks for returning nothing.
+    def exchange(self, nums, i, j):
+        tmp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = tmp
 
 
 
-Sol = Solution()
 nums = [2, 9, 8, 7, 2, 1, 5, 6, 4]
-print Sol.wiggleSort(nums)
-
+Solution().wiggleSort(nums)
+print nums
 
 
 
