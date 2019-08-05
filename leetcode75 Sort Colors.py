4c634b60d1bec51b6a946a7aeb81af8618197594
Hao Wu <how@amazon.com>
2019-08-05 01:13:41 -0700

    add changes

--- leetcode75 Sort Colors.py
+++ leetcode75 Sort Colors.py
@@ -23,37 +23,31 @@ class Solution(object):
                 nums[i] = 0
                 i += 1
 
-        #return nums    # leetcode asks to return nothing
-
 
 
 nums = [0, 1, 2, 2, 1, 0, 2, 1, 1, 2]
-Sol = Solution()
-print Sol.sortColors(nums)
+Solution().sortColors(nums)
+print nums
 
 
 
 '''
-# naive counting sort solution, based on that we already know what value we need:
+from collections import Counter
+
+# naive counting sort solution
 
 class Solution(object):
     def sortColors(self, nums):
 
-        count0 = count1 = count2 = 0
-        for num in nums:
-            if num == 0: count0 += 1
-            if num == 1: count1 += 1
-            if num == 2: count2 += 1
-
-        count1, count2 = count0 + count1, count0 + count1 + count2
-        i = 0
-        while i < count0:
-            nums[i] = 0
-            i += 1
-        while i < count1:
-            nums[i] = 1
-            i += 1
-        while i < count2:
-            nums[i] = 2
-            i += 1
+        numCount = Counter(nums)
+        for i in xrange(len(nums)):
+            if numCount[0] > 0:
+                nums[i] = 0
+                numCount[0] -= 1
+            elif numCount[1] > 0:
+                nums[i] = 1
+                numCount[1] -= 1
+            else:
+                nums[i] = 2
+                numCount[2] -= 1
 '''
