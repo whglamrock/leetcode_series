1aecccf49496404750b86104aeb38c42dae0f203
Hao Wu <how@amazon.com>
2019-08-10 20:04:22 -0700

    add changes

--- leetcode124 Binary Tree Maximum Path Sum.py
+++ leetcode124 Binary Tree Maximum Path Sum.py
@@ -1,30 +1,44 @@
+
 # Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-class TreeNode(object):
-    def __init__(self, x):
-
-        self.val = x
-        self.left = None
-        self.right = None
-
-
-# the idea for maxPathDown function is the input node is highest node in path
+# the key is to use dfs to get the max path sum that ends with node i
 
 class Solution(object):
     def maxPathSum(self, root):
-
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
         self.maxSum = -2147483648
-
-        def maxPathDown(node):
-
-            if not node: return 0
-            leftmax = max(0, maxPathDown(node.left))
-            rightmax = max(0, maxPathDown(node.right))
-            self.maxSum = max(self.maxSum, node.val + leftmax + rightmax)
-
-            return max(leftmax, rightmax) + node.val
-
-        maxPathDown(root)
+        # each node stores the biggest sum that ends with it
+        self.dfs(root)
         return self.maxSum
+
+    def dfs(self, node):
+        maxToNode = node.val
+        maxLeft = 0
+        maxRight = 0
+
+        if node.left:
+            maxLeft = self.dfs(node.left)
+        if node.right:
+            maxRight = self.dfs(node.right)
+
+        maxToNode = max(maxToNode,
+                        node.val + maxLeft,
+                        node.val + maxRight)
+
+        self.maxSum = max(self.maxSum,
+                          node.val,
+                          maxLeft + node.val,
+                          maxRight + node.val,
+                          maxLeft + maxRight + node.val)
+
+        return maxToNode
 
 
