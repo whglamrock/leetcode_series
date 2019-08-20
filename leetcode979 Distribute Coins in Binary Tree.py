02d98c9ac4acbfed7040f37e6796428e7f5949d5
Hao Wu <how@amazon.com>
2019-08-20 10:48:49 -0700

    add changes

--- leetcode979 Distribute Coins in Binary Tree.py
+++ leetcode979 Distribute Coins in Binary Tree.py
@@ -9,6 +9,11 @@ class TreeNode(object):
 
 
 
+# idea from: https://leetcode.com/problems/distribute-coins-in-binary-tree/solution/, notice the intuition part
+
+# we count the number of moves needed for each NODE, and we use DFS from bottom to top to return the state after
+# each subtree has completed moves.
+
 class Solution(object):
     def distributeCoins(self, root):
         """
@@ -18,10 +23,14 @@ class Solution(object):
         self.ans = 0
 
         def dfs(node):
+
             if not node:
                 return 0
+
             L, R = dfs(node.left), dfs(node.right)
             self.ans += abs(L) + abs(R)
+
+            # the L & R can offset each other, so we we count the number of moves
             return (node.val - 1) + L + R
 
         dfs(root)
