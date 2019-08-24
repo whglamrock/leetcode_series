1247bf02e7f04b39ba4679f72d020bd7e497110e
Hao Wu <how@amazon.com>
2019-08-24 14:18:37 -0700

    add changes

--- leetcode273 Integer to English Words.py
+++ leetcode273 Integer to English Words.py
@@ -1,46 +1,48 @@
 
-# the hint is to devide num into triplets so we can deal with them one by one
-# P.S., there is no need to add 'self.' before the LESSTHAN20, etc.
+# The most important tip to remember is divide the number into triplets and use recursion with the helper funciton
 
 class Solution(object):
     def numberToWords(self, num):
-
-        if num == None: return ''
-        if num == 0: return 'Zero'
-
-        # notice how each digit array is constructed. The goal is to use it conveniently
-        LESSTHAN20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
-        TENS = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
-        THOUSANDS = ["", "Thousand", "Million", "Billion"]
-
-        # just to remember that every return value of helper function has space at the end
-        #   so there is no need to add space before 'Thousands[i]'
-        def helper(triplet):
-
-            # very necessary, to avoid add additional space if triplet == 0, try test case: num == 50688
-            if triplet == 0:
-                return ''
-            elif triplet < 20:
-                return LESSTHAN20[triplet] + ' '
-            elif triplet < 100:
-                return TENS[triplet / 10] + ' ' + helper(triplet % 10)
-                # <=> return TENS[triplet / 10] + ' ' + (LESSTHAN20[triplet % 10] + ' ' if triplet % 10 > 0 else '')
-            else:
-                return LESSTHAN20[triplet / 100] + ' Hundred ' + helper(triplet % 100)
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num == None:
+            return ''
+        if num == 0:
+            return 'Zero'
 
         i = 0
         ans = ''
-        while num > 0:
-            # notice when num == 1000000 and why the if statement is necessary
+        while num:
+            # very important! To filter out the '000's.
             if num % 1000 > 0:
-                ans = helper(num % 1000) + THOUSANDS[i] + ' ' + ans
-            num /= 1000
+                ans = self.convertChunk(num % 1000) + self.THOUSANDS[i] + ' ' + ans
             i += 1
+            num /= 1000
 
         return ans.rstrip(' ')
 
+    def convertChunk(self, num):
+        if num == 0:
+            return ''
+        elif num < 20:
+            return self.LESSTHAN20[num] + ' '
+        elif num < 100:
+            return self.TENS[num / 10] + ' ' + self.convertChunk(num % 10)
+        else:
+            return self.LESSTHAN20[num / 100] + ' Hundred ' + self.convertChunk(num % 100)
+
+    def __init__(self):
+        self.LESSTHAN20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+                           "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        self.TENS = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        # since the num can be at most 2147483647
+        self.THOUSANDS = ["", "Thousand", "Million", "Billion"]
 
 
-Sol = Solution()
-num = 1234567890
-print Sol.numberToWords(num)
+
+print Solution().numberToWords(1234567891)
+print Solution().numberToWords(1000000000)
+print Solution().numberToWords(1000120101)
+print Solution().numberToWords(10000)
