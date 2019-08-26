d3e26ee299940263777547e6389d24774e12200b
Hao Wu <how@amazon.com>
2019-08-26 15:39:54 -0700

    add changes

--- leetcode329 Longest Increasing Path in a Matrix.py
+++ leetcode329 Longest Increasing Path in a Matrix.py
@@ -1,7 +1,85 @@
 
-# a very special dp practice.
-# pay attention to how the recursion is formed.
+# worst case O(V^2) solution, where V is number of vertex (V = m * n); but in reality it's better than V^2
 
+class Solution(object):
+
+    def longestIncreasingPath(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+        if not matrix or not matrix[0]:
+            return 0
+
+        m, n = len(matrix), len(matrix[0])
+        memo = set()
+
+        for i in xrange(m):
+            for j in xrange(n):
+                if self.isLocalMinimum(matrix, i, j):
+                    memo.add((i, j))
+
+        lengthOfLongest = 0
+        while memo:
+            lengthOfLongest += 1
+            nextMemo = set()
+            for i, j in memo:
+                nextIncreasing = self.findNextIncreasingPoints(matrix, i, j)
+                nextMemo |= nextIncreasing
+            memo = nextMemo
+
+        return lengthOfLongest
+
+    def isLocalMinimum(self, matrix, i, j):
+        if i - 1 >= 0 and matrix[i - 1][j] < matrix[i][j]:
+            return False
+        if i + 1 < len(matrix) and matrix[i + 1][j] < matrix[i][j]:
+            return False
+        if j - 1 >= 0 and matrix[i][j - 1] < matrix[i][j]:
+            return False
+        if j + 1 < len(matrix[0]) and matrix[i][j + 1] < matrix[i][j]:
+            return False
+        return True
+
+    def findNextIncreasingPoints(self, matrix, i, j):
+        nextIncreasing = set()
+        if i - 1 >= 0 and matrix[i - 1][j] > matrix[i][j]:
+            nextIncreasing.add((i - 1, j))
+        if i + 1 < len(matrix) and matrix[i + 1][j] > matrix[i][j]:
+            nextIncreasing.add((i + 1, j))
+        if j - 1 >= 0 and matrix[i][j - 1] > matrix[i][j]:
+            nextIncreasing.add((i, j - 1))
+        if j + 1 < len(matrix[0]) and matrix[i][j + 1] > matrix[i][j]:
+            nextIncreasing.add((i, j + 1))
+
+        return nextIncreasing
+
+
+
+print Solution().longestIncreasingPath([
+  [9,9,4],
+  [6,6,8],
+  [2,1,1]
+])
+print Solution().longestIncreasingPath([
+  [3,4,5],
+  [3,2,6],
+  [2,2,1]
+])
+print Solution().longestIncreasingPath([
+  [2,2,2],
+  [2,2,2],
+  [2,2,2]
+])
+print Solution().longestIncreasingPath([
+  [2,2,2],
+  [2,2,2],
+  [2,0,1]
+])
+
+
+
+'''
 class Solution(object):
     def longestIncreasingPath(self, matrix):
 
@@ -22,3 +100,4 @@ class Solution(object):
             return dp[i][j]
 
         return max(dfs(x, y) for x in xrange(m) for y in xrange(n))
+'''
