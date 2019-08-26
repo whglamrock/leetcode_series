2474be332b78304b637e66acf6f108f1aa76b763
Hao Wu <how@amazon.com>
2019-08-26 16:59:44 -0700

    add changes

--- leetcode210 Course Schedule II.py
+++ leetcode210 Course Schedule II.py
@@ -1,46 +1,47 @@
 
+from collections import defaultdict, deque
+
 # topological sort solution
 # idea from lc207
 
-import collections
 class Solution(object):
     def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        less, greater = self.buildRelationship(prerequisites)
 
-        if numCourses == 0: return []
-        if not prerequisites or len(prerequisites) == 0:
-            return [i for i in xrange(numCourses)]
-
-        less = collections.defaultdict(set)
-        greater = collections.defaultdict(set)
-
-        for item in prerequisites:
-            less[item[1]].add(item[0])
-            greater[item[0]].add(item[1])
-
-        queue = collections.deque()
+        q = deque()
         for i in xrange(numCourses):
             if i not in greater:
-                queue.append(i)
+                q.append(i)
 
-        order = []
-        while queue:
-            i = queue.popleft()
-            if i in less:
-                for j in less[i]:
-                    greater[j].discard(i)
-                    if len(greater[j]) == 0:
-                        del greater[j]
-                        queue.append(j)
-            order.append(i)
+        ans = []
+        while q:
+            j = q.popleft()
+            ans.append(j)
+            # this means no course depends on j
+            if j not in less:
+                continue
+            for i in less[j]:
+                greater[i].discard(j)
+                if not greater[i]:
+                    del greater[i]
+                    q.append(i)
 
-        if len(greater) > 0:
-            return []
-        else:
-            return order
+        return ans if not greater else []
+
+    def buildRelationship(self, prerequisites):
+        less, greater = defaultdict(set), defaultdict(set)
+        # j is i's prerequisite
+        for i, j in prerequisites:
+            less[j].add(i)
+            greater[i].add(j)
+
+        return less, greater
 
 
 
-Sol = Solution()
-numCourses = 2
-prerequisites = [[1,0]]
-print Sol.findOrder(numCourses, prerequisites)
+print Solution().findOrder(4, [[1,0],[2,0],[3,1],[3,2]])
