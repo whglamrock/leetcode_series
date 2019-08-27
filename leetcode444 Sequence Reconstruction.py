c818ff3063f14779450631702c1cfc37babc5202
Hao Wu <how@amazon.com>
2019-08-26 17:59:13 -0700

    add changes

--- leetcode444 Sequence Reconstruction.py
+++ leetcode444 Sequence Reconstruction.py
@@ -1,52 +1,60 @@
 
-import collections
+from collections import defaultdict, deque
+
+# let's see some bullshit edge cases leetcode gives:
+    # 1. [1], [[1],[1],[1]]
+    # 2. [1], [[],[]]
+    # 3. [1], []
+    # 4. [1], [[1],[2,3],[3,2]]
 
 class Solution(object):
     def sequenceReconstruction(self, org, seqs):
-
-        # if either of these two is empty, another must be empty.
-        if not org:
-            return not seqs
+        """
+        :type org: List[int]
+        :type seqs: List[List[int]]
+        :rtype: bool
+        """
         if not seqs:
             return not org
-        # for corner case like: org = [1], seq = [[1,1]] or seq = [[1], [1], [1]]
-        # or seq = [[1], [1], [2]]
-        if len(org) == 1:
-            for seq in seqs:
-                if seq != org: return False
-            return True
 
-        nodes = set(org)
-        edges = collections.defaultdict(set)
+        less, greater = self.buildRelationship(seqs)
+
+        # using a numSet to gather all numbers occurred in seqs
+        numSet = set()
         for seq in seqs:
-            # seq could be []
-            if not seq: continue
-            # a disconnected node that won't be added to edges but we need to check it
-            if seq[0] not in nodes: return False
-            n = len(seq)
-            # if len(seq) == 1, this for loop won't be excuted
-            for i in xrange(n - 1):
-                edges[seq[i]].add(seq[i + 1])
+            for num in seq:
+                numSet.add(num)
+        # the topology queue is based on numSet and ans constructed from it will be used to compare with org
+        q = deque()
+        for num in numSet:
+            if num not in greater:
+                q.append(num)
 
-        print edges
-
-        # won't cover the case when len(org) == 1.
-        for i in xrange(len(org) - 1):
-            node, next = org[i], org[i + 1]
-            if node not in edges or next not in edges[node]:
+        ans = []
+        while q:
+            if len(q) != 1:
                 return False
-            del edges[node]
-            print edges
+            i = q.popleft()
+            ans.append(i)
+            if i in less:
+                for j in less[i]:
+                    greater[j].discard(i)
+                    if not greater[j]:
+                        del greater[j]
+                        q.append(j)
 
-        # for case like org = [1,2,3,4], seqs = [[1,2,3], [2,3], [3,4], [4,3]],
-        # the edges could still not be empty in the end, so we need to check it
-        # instead of directly returning True
-        return not edges
+        return ans == org and not greater
+
+    def buildRelationship(self, seqs):
+        less, greater = defaultdict(set), defaultdict(set)
+        # i is lower than j
+        for seq in seqs:
+            for i in xrange(len(seq) - 1):
+                less[seq[i]].add(seq[i + 1])
+                greater[seq[i + 1]].add(seq[i])
+        return less, greater
 
 
 
-org = [1, 2, 3]
-seq = [[1,2], [1,3]]
-Sol = Solution()
-print Sol.sequenceReconstruction(org, seq)
-
+print Solution().sequenceReconstruction([1, 2, 3], [[1, 2], [1, 3]])
+print Solution().sequenceReconstruction([4, 1, 5, 2, 6, 3], [[5, 2, 6, 3], [4, 1, 5, 2]])
