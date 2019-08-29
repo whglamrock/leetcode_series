7f348da58b63e22b7566572f58f4328eef8ef39d
Hao Wu <how@amazon.com>
2019-08-29 11:59:57 -0700

    add changes

--- leetcode23 Merge k Sorted Lists.py
+++ leetcode23 Merge k Sorted Lists.py
@@ -1,8 +1,6 @@
 
 # Definition for singly-linked list.
 
-from heapq import *
-
 class ListNode(object):
     def __init__(self, x):
 
@@ -10,25 +8,39 @@ class ListNode(object):
         self.next = None
 
 
-# this way is actually much more efficient because the average size of the heap is len(lists)
-#   and it is guaranteed that the nodes come into the heap in a sorted order
+
+from heapq import *
+
+# O(N * logK) solution, where K is len(lists), N is the total number of nodes in all lists
 
 class Solution(object):
     def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if not lists:
+            return None
+
+        # use a dummy head to avoid checking "if not root: root = node" in the while loop
+        dummy = ListNode(None)
+        tmp = dummy
 
         pq = []
-        dummy = ListNode(0)
-        temp = dummy
-
+        heapify(pq)
+        # initialize the pq
         for root in lists:
+            # leetcode does give the corner case like [[]]
             if root:
                 heappush(pq, (root.val, root))
+
         while pq:
-            value, node = heappop(pq)
+            val, node = heappop(pq)
             if node.next:
                 heappush(pq, (node.next.val, node.next))
-            temp.next = node
-            temp = temp.next
+            tmp.next = node
+            node.next = None
+            tmp = tmp.next
 
         return dummy.next
 
@@ -46,39 +58,10 @@ f = ListNode(4)
 d.next = e
 e.next = f
 
-lst = [a,d]
-Sol = Solution()
-value = Sol.mergeKLists(lst)
-print value.val
-
-
-
-'''
-# naive old version:
-
-class Solution(object):
-    def mergeKLists(self, lists):
-
-        res = []
-        for l in lists:
-            while l:
-                res.append((l.val, l))
-                l = l.next
-
-        res.sort(key=lambda tup: tup[0])
-
-        dummy = ListNode(0)
-        temp = dummy
-
-        for l in res:
-            dummy.next = l[1]
-            dummy = dummy.next
-
-        return temp.next
-'''
-
-
-
+root = Solution().mergeKLists([a,d])
+while root:
+    print root.val
+    root = root.next
 
 
 
---
+++ leetcode718 Maximum Length of Repeated Subarray.py
@@ -0,0 +1,29 @@
+
+# essentially looking for longest common substring
+
+class Solution(object):
+    def findLength(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: int
+        """
+
+        m, n = len(A), len(B)
+        dp = [[0 for j in xrange(n + 1)] for i in xrange(m + 1)]
+
+        for i in xrange(1, m + 1):
+            for j in xrange(1, n + 1):
+                if A[i - 1] == B[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+
+        maxLen = 0
+        for i in xrange(1, m + 1):
+            for j in xrange(1, n + 1):
+                maxLen = max(maxLen, dp[i][j])
+
+        return maxLen
+
+
+
+print Solution().findLength([1,2,3,2,1], [3,2,1,4,7])
