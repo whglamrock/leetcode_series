2d8ef8f6586c110a80709089649999ff8857e568
Hao Wu <how@amazon.com>
2019-08-29 18:43:52 -0700

    add changes

--- leetcode72 Edit Distance.py
+++ leetcode72 Edit Distance.py
@@ -1,35 +1,42 @@
 
-# the key is to find the optimal substructure of dp
+# the key is to define dp[i][j], and which previous dp[i][j] we should use for each option
 # idea from: https://discuss.leetcode.com/topic/20922/java-dp-solution-o-nm
 
 class Solution(object):
     def minDistance(self, word1, word2):
-
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
         m, n = len(word1), len(word2)
-        # in the dp table, dp[i][j] represents min steps
-        #   to match word1[:i] with word2[:j]
+        # dp[i][j] means the min steps for word1[:i] to match word2[:j]
         dp = [[2147483647 for j in xrange(n + 1)] for i in xrange(m + 1)]
+        dp[0][0] = 0
 
-        for i in xrange(m + 1):
+        # there is no other option for word1 to match with an empty string
+        for i in xrange(1, m + 1):
             dp[i][0] = i
-        for j in xrange(n + 1):
+
+        # there is no other option for word2 to match with an empty string
+        for j in xrange(1, n + 1):
             dp[0][j] = j
 
-        # the analysis is top-down, but to avoid recursion, we use bottom-up approach
         for i in xrange(1, m + 1):
             for j in xrange(1, n + 1):
                 if word1[i - 1] == word2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
+                # compare 3 options
                 else:
-                    # replace the char of word1[i - 1] with word2[j - 1]
-                    a = dp[i - 1][j - 1]
-                    # delete a char at word1[i - 1], let word1[:i] to match word2[:j + 1]
+                    # insert a char (word2[j - 1]) at the end of word1's permutation, in this
+                    # case we need the min steps for matching word1[:i] with word2[:j - 1]
+                    a = dp[i][j - 1]
+                    # delete a char; in this case we need the min steps for matching
+                    # word1[:i - 1] with word2[:j]
                     b = dp[i - 1][j]
-                    # let the original word1[:i + 1] to match word2[:j], and
-                        # insert a new char to match word2[j]
-                    c = dp[i][j - 1]
-                    dp[i][j] = min(a, b, c)
-                    dp[i][j] += 1
+                    # replace a char
+                    c = dp[i - 1][j - 1]
+                    dp[i][j] = min(a, b, c) + 1
 
         return dp[-1][-1]
 
