0fecfeb0f94f2818d5f9d01fdabeaa70b09f010d
Hao Wu <how@amazon.com>
2019-08-30 23:56:17 -0700

    add changes

--- leetcode1 Two Sum.py
+++ leetcode1 Two Sum.py
@@ -2,9 +2,7 @@
 # O(N) hash-table solution
 
 class Solution(object):
-
     def twoSum(self, nums, target):
-
         if nums is None or not nums:
             return []
 
@@ -15,39 +13,3 @@ class Solution(object):
             valueToIndex[num] = i
 
         return []
-
-
-
-'''
-# in lintcode, several edge cases need to be considered.
-
-class Solution:
-
-    def twoSum(self, nums, target):
-        # write your code here
-        if (not nums): return []
-
-        dick = {}
-        for i in xrange(len(nums)):
-            if nums[i] not in dick:
-                dick[nums[i]] = [i]
-            else:
-                dick[nums[i]].append(i)
-
-        ans = []
-        for num in dick:
-            if target - num in dick:
-                if target - num == num:
-                    if num == 0 and len(dick[num]) == 1:   # this doesn't but double 0s work
-                        continue
-                    elif len(dick[num]) > 1:  # if len(dick[num]) == 1, we can't choose num
-                        ans.append(dick[num][0])
-                        ans.append(dick[num][1])
-                elif target - num != num:    # normal case
-                    ans.append(dick[num][0])
-                    ans.append(dick[target-num][0])
-                break
-
-        ans.sort()
-        return ans
-'''
