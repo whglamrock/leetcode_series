e2d2a246282814df6f5dfdf29a939214f109bd46
Hao Wu <how@amazon.com>
2019-08-30 18:30:42 -0700

    add changes

--- leetcode315 Count of Smaller Numbers After Self.py
+++ leetcode315 Count of Smaller Numbers After Self.py
@@ -1,38 +1,50 @@
 
-# An actual divide-and-conquer solution, or... mergesort solution
+from collections import deque
+
+# redefine the question to a binary index tree question:
+# every number has a rank: the count of numbers smaller than ranks(4) = count of
+# numbers at ranks(3) + count of numbers at ranks(2) + count of numbers at ranks(1).
+# => this is essentially queryBIT(3).
+# Naturally we make ranks(i) = 1.
+# if we do this in reverse order, we can get the answer
 
 class Solution(object):
     def countSmaller(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+        n = len(nums)
 
-        if not nums or len(nums) == 1:
-            return 0
+        # BIT is non-0 index based array because we need to perform bit operation on index
+        ranks = {num: rank + 1 for rank, num in enumerate(sorted(nums))}
+        bit = [0] * (n + 1)
 
-        smaller = [0] * len(nums)
+        res = deque()
+        for num in nums[::-1]:
+            # we need smaller numbers so use rank - 1 (BIT "prefix sum" is inclusive)
+            count = self.queryBIT(bit, ranks[num] - 1)
+            res.appendleft(count)
+            self.updateBIT(bit, ranks[num])
 
-        def sort(enum):  # need to pass the actual list, not just index.
+        return res
 
-            if not enum:
-                return
-            if len(enum) == 1:
-                return enum
+    # no diff in input here because we always add 1
+    def updateBIT(self, bit, i):
+        while i < len(bit):
+            bit[i] += 1
+            i += (i & -i)
 
-            half = len(enum) / 2
-            left, right = sort(enum[:half]), sort(enum[half:])
-            # from last to first, find the max of the remaining.
-            for i in xrange(len(enum) - 1, -1, -1):
-                if (not right) or (left and left[-1][-1] > right[-1][-1]):
-                    # left[-1][-1] bigger than the whole right list
-                    smaller[left[-1][0]] += len(right)
-                    enum[i] = left.pop()
-                else:
-                    enum[i] = right.pop()
-            return enum
-
-        sort(list(enumerate(nums)))
-        #print nums
-        return smaller
+    def queryBIT(self, bit, i):
+        ans = 0
+        while i > 0:
+            ans += bit[i]
+            i -= (i & -i)
+        return ans
 
 
 
 Sol = Solution()
-print Sol.countSmaller([5,6,2,1,3,4,7,8,10])
+print Sol.countSmaller([5, 6, 2, 1, 3, 4, 7, 8, 10])
