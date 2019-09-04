b4897747dac072e74979c37dc5699df3b464763f
Hao Wu <how@amazon.com>
2019-09-03 23:03:38 -0700

    add changes

--- leetcode212 Word Search II.py
+++ leetcode212 Word Search II.py
@@ -1,77 +1,64 @@
 
-# the idea of using trie is to 'save' the space.
-# Trie in worst case takes O(26^m) space complexity where the m is the length of longest word.
-# Using a hashset to keep the words takes O(k*n) space where the k is the average length of word
-#   and the n is number of words.
+# The idea of using trie is to 'save' the space. Trie in worst case takes O(26^M) space, where M
+    # is the length of longest word.
 
-from _collections import defaultdict
-from copy import deepcopy
+# Using a hashset to keep the words takes O(M*N) space where the M is the average length of word
+    # and the N is number of words.
 
-class TrieNode(object):
+from collections import defaultdict
+
+class TrieNode:
     def __init__(self):
-        self.isword = False
+        self.isWord = False
         self.children = defaultdict(TrieNode)
 
 
-class Trie(object):
-    def __init__(self):
-        self.root = TrieNode()
-
-    def addword(self, word):
-        cur = self.root
-        for letter in word:
-            cur  = cur.children[letter]
-        cur.isword = True
-
-# Directly used the Trie implementation from lc208. However, the 'searchword' function
-# has beeN removed because it's useless in this question.
-# DFS solution. Discussing the time/space doesn't make much sense here: even
-# with optimization the time could still be exponential in worst case
 
 class Solution(object):
     def findWords(self, board, words):
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        if not words or not board or not board[0]:
+            return []
 
-        if not board or len(board[0]) == 0 or (not words): return []
-
-        self.board = deepcopy(board)
-        self.res = []
-        self.trie = Trie()
-
+        root = TrieNode()
         for word in words:
-            self.trie.addword(word)
+            curr = root
+            for c in word:
+                curr = curr.children[c]
+            curr.isWord = True
 
+        self.ans = set()
         for i in xrange(len(board)):
             for j in xrange(len(board[0])):
-                # if we make the path a list, it will be slower than a string
-                self.dfs(i, j, self.trie.root, '')
+                self.dfs(i, j, root, board, '')
 
-        return self.res
+        return list(self.ans)
 
-    def dfs(self, i, j, node, path):
+    def dfs(self, i, j, curr, board, path):
+        if curr.isWord:
+            self.ans.add(path)
 
-        if node.isword:
-            self.res.append(path)
-            # the words input in findwords function might contain duplicates
-            node.isword = False
-
-        # invalid input
-        if i < 0 or i >= len(self.board) or j < 0 or j >= len(self.board[0]):
+        m, n = len(board), len(board[0])
+        # we do check here instead of right above the dfs, because we need to consider
+        # corner case like: [["a"]], ["a"], in which case if we check boundary right
+        # before next dfs we won't be able to reach it (but we need to reach the next
+        # dfs to trigger the "curr.isWord")
+        if i < 0 or i >= m or j < 0 or j >= n:
+            return
+        if board[i][j] not in curr.children:
             return
 
-        tmp = self.board[i][j]
-        if tmp not in node.children:
-            return
-        node = node.children[tmp]
-        # this step has to be after the if statement check because if the program ends, the
-        # self.board[i][j] = '#' will be changed permanently
-        self.board[i][j] = '#'  # or it can be any character other than lower letters
+        # way easier to keep a visited list
+        tmp = board[i][j]
+        board[i][j] = '#'
 
-        # the performance of using string for path is better, probably because the
-        # recursion needs to pass the list path to next recursion, which is more time
-        # consuming than passing a string
-        self.dfs(i - 1, j, node, path + tmp)
-        self.dfs(i + 1, j, node, path + tmp)
-        self.dfs(i, j - 1, node, path + tmp)
-        self.dfs(i, j + 1, node, path + tmp)
-        self.board[i][j] = tmp
+        self.dfs(i - 1, j, curr.children[tmp], board, path + tmp)
+        self.dfs(i + 1, j, curr.children[tmp], board, path + tmp)
+        self.dfs(i, j - 1, curr.children[tmp], board, path + tmp)
+        self.dfs(i, j + 1, curr.children[tmp], board, path + tmp)
 
+        board[i][j] = tmp
