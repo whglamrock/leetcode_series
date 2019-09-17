352293baab0088290da1c728d97f61ebef204374
Hao Wu <how@amazon.com>
2019-09-16 17:33:09 -0700

    add changes

--- leetcode374 Guess Number Higher or Lower.py
+++ leetcode374 Guess Number Higher or Lower.py
@@ -4,20 +4,23 @@
 # @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
 # def guess(num):
 
-# the goal is to track down 'my' number
-
 class Solution(object):
     def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
 
         l, r = 1, n
         while l <= r:
-            mid = (l+r)/2
-            if guess(mid) == -1:  # 'my' number is lower, means the goal should be on the left side
-                r = mid-1
-            elif guess(mid) == 1:  # 'my' number is higher, means the goal should be on the right side
-                l = mid+1
+            m = (l + r) / 2
+            if guess(m) == 0:
+                return m
+            # m < target
+            elif guess(m) == 1:
+                l = m + 1
             else:
-                return mid
-
-        return l
+                r = m - 1
 
