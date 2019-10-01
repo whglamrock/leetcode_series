a78ebf59a25cdd8895f49a6d8a45cdfad63a3e39
Hao Wu <how@amazon.com>
2019-10-01 12:02:03 -0700

    add changes

--- leetcode261 Graph Valid Tree.py
+++ leetcode261 Graph Valid Tree.py
@@ -1,56 +1,38 @@
 
-# Typical unionfind solution
+# Typical union find solution. remember the find() and union() methods
 
 class Solution(object):
     def validTree(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: bool
+        """
+        if n == 0:
+            return False
+        if not edges:
+            return n <= 1
 
-        if not edges: return n == 1
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
 
-        self.unionid = 0
-        dic = {}
-        uniondic = {}
-        self.flag = True
+        def union(x, y):
+            # when we union 2 parts(each part contains a bunch of nodes) of the tree, the following line won't
+                # update all parents of x set to find(y);
+            # i.e., it will only compress the path in x set first then do the union
+            parent[find(x)] = find(y)
 
-        def unionfind(a, b):
-            #print dic, uniondic
-            if a not in dic and b not in dic:
-                dic[a] = self.unionid
-                dic[b] = self.unionid
-                uniondic[self.unionid] = {a, b}
-                self.unionid += 1
-            elif a in dic and b not in dic:
-                id = dic[a]
-                dic[b] = id
-                uniondic[id].add(b)
-            elif b in dic and a not in dic:
-                id = dic[b]
-                dic[a] = id
-                uniondic[id].add(a)
-            else:
-                ida = dic[a]
-                idb = dic[b]
-                if ida == idb: self.flag = False
-                # in the following process, every elment will be unioned at most once,
-                # so the time complexity is still O(N)
-                if len(uniondic[ida]) > len(uniondic[idb]):
-                    for item in uniondic[idb]:
-                        uniondic[ida].add(item)
-                        dic[item] = ida
-                    del uniondic[idb]
-                else:
-                    for item in uniondic[ida]:
-                        uniondic[idb].add(item)
-                        dic[item] = idb
-                    del uniondic[ida]
+        parent = range(n)
+        for i, j in edges:
+            x = find(i)
+            y = find(j)
+            # circle exists
+            if x == y:
+                return False
+            union(i, j)
+            # print parent
 
-        for edge in edges:
-            a, b = edge
-            unionfind(a, b)
-            # if there is a cycle, means all elements in the cycle is already deleted in uniondic
-            # to avoid unnecessary key error, we directly return False here
-            if not self.flag: return False
-
-        if len(uniondic) != 1: return False   # not connected
-        if len(dic) != n: return False    # there are nodes connected by no edges
-        return True
+        return len(edges) == n - 1
 
