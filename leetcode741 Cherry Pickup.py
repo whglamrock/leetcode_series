10cf68311ba2685cd6d43b7f37f0faac55ac23c9
Hao Wu <how@amazon.com>
2019-10-01 14:30:46 -0700

    add changes

--- leetcode741 Cherry Pickup.py
+++ leetcode741 Cherry Pickup.py
@@ -1,36 +1,40 @@
 
+# A hard level question. It's impractical to use the DP approach because we have to store the state of whole grid in
+    # each loop. A recursive DFS solution is easier and more practical for interview
+
 class Solution(object):
     def cherryPickup(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        ans = self.dfs(0, 0, 0, 0, grid, {})
+        return ans if ans > 0 else 0
 
-        memo = {}
-        res = self.dfs(0, 0, 0, 0, grid, memo)
-        return res if res > 0 else 0
-
+    # find two paths (can overlap) that have max sum of cherries
     def dfs(self, x1, y1, x2, y2, grid, memo):
         n = len(grid)
+
         # check if we reached bottom right corner; (x1, y1), (x2, y2) will reach bottom right at the same time
         if x1 == n - 1 and y1 == n - 1:
-            return grid[x1][y1] if grid[x1][y1] != -1 else -2147483648
+            return grid[-1][-1] if grid[-1][-1] >= 0 else -2147483647
 
-        # out of the grid and thorn check
-        if x1 == n or y1 == n or x2 == n or y2 == n or grid[x1][y1] == -1 or grid[x2][y2] == -1:
-            return -2147483648
+        if x1 >= n or y1 >= n or x2 >= n or y2 >= n or grid[x1][y1] == -1 or grid[x2][y2] == -1:
+            return -2147483647
 
-        # memorization check
         if (x1, y1, x2, y2) in memo:
             return memo[(x1, y1, x2, y2)]
 
-        # pick your cherries
         if x1 == x2 and y1 == y2:
             cherries = grid[x1][y1]
         else:
             cherries = grid[x1][y1] + grid[x2][y2]
 
         res = cherries + max(
-            self.dfs(x1 + 1, y1, x2 + 1, y2, grid, memo),  # right, right
-            self.dfs(x1, y1 + 1, x2, y2 + 1, grid, memo),  # down, down
-            self.dfs(x1 + 1, y1, x2, y2 + 1, grid, memo),  # right, down
-            self.dfs(x1, y1 + 1, x2 + 1, y2, grid, memo)  # down, right
+            self.dfs(x1 + 1, y1, x2 + 1, y2, grid, memo),
+            self.dfs(x1 + 1, y1, x2, y2 + 1, grid, memo),
+            self.dfs(x1, y1 + 1, x2 + 1, y2, grid, memo),
+            self.dfs(x1, y1 + 1, x2, y2 + 1, grid, memo)
         )
 
         memo[(x1, y1, x2, y2)] = res
