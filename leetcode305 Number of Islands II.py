595ba94bfeb6127995b7c71a97b3ea6566cf1724
Hao Wu <how@amazon.com>
2019-10-02 17:34:57 -0700

    add changes

--- leetcode305 Number of Islands II.py
+++ leetcode305 Number of Islands II.py
@@ -1,63 +1,63 @@
 
-# Intro to UnionFind find: https://www.youtube.com/watch?v=ID00PMy0-vE&t=761s
-# A naive solution takes O(K * log(M * N)) time complexity, which satisfies the interview standard. But in real
-    # interview it would be nice to mention that with proper "union by rank & path compression"
-    # each union operation can take amortized O(log(M * N))
+# remember how the find() & union() helper methods are used in 2D condition.
+# Time complexity O(k * log(m * n)) where k == len(positions). It's mathematically proved that with path compression
+    # union() and find() both take O(log(m * n))
 
 class Solution(object):
     def numIslands2(self, m, n, positions):
+        """
+        :type m: int
+        :type n: int
+        :type positions: List[List[int]]
+        :rtype: List[int]
+        """
+        if not positions:
+            return []
 
+        def find(node):
+            if parent[node] != node:
+                parent[node] = find(parent[node])
+            return parent[node]
+
+        def union(node1, node2):
+            parent[find(node1)] = find(node2)
+
+        parent = {}
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        numOfIslands = 0
         ans = []
-        island = UnionFind()
-        for position in positions:
-            i, j = position
-            island.add((i, j))
-            for dir in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                newpo = (i + dir[0], j + dir[1])
-                if newpo in island.parent:
-                    island.union((i, j), newpo)
-            ans.append(island.count)
+
+        for i, j in positions:
+            if (i, j) in parent:  # leetcode does give this stupid test case
+                ans.append(numOfIslands)
+                continue
+
+            # find all the connected islands(if exists)
+            islandSet = set()
+            for x, y in directions:
+                if (i + x, j + y) in parent:
+                    islandSet.add(find((i + x, j + y)))
+
+            # modify the numOfIslands and append it to ans
+            numOfIslands -= len(islandSet)
+            numOfIslands += 1
+            ans.append(numOfIslands)
+
+            # union the connected islands
+            islands = list(islandSet)
+            for k in xrange(len(islands) - 1):
+                union(islands[k], islands[k + 1])
+
+            # make sure parent[(i, j)] is also up to date
+            if islands:
+                # not directly islands[0] because it could have changed with the union() method
+                parent[(i, j)] = find(islands[0])
+            else:
+                parent[(i, j)] = (i, j)
 
         return ans
 
 
 
-# the classic way of defining a UnionFind object
-
-class UnionFind(object):
-    def __init__(self):
-
-        self.parent = {}
-        # how many nodes are under a specific findParent
-        self.nodeToNumOfChildren = {}
-        self.count = 0
-
-    def add(self, p):
-
-        self.parent[p] = p
-        self.count += 1
-        self.nodeToNumOfChildren[p] = 1
-
-    def findParent(self, i):
-
-        while self.parent[i] != i:
-            self.parent[i] = self.parent[self.parent[i]]    # path compression
-            i = self.parent[i]
-        return i
-
-    def union(self, p, q):  # in the problem, p and q are presented as tuples.
-
-        p = self.findParent(p)
-        q = self.findParent(q)
-        if p == q:
-            return
-        if self.nodeToNumOfChildren[p] < self.nodeToNumOfChildren[q]:
-            p, q = q, p
-        self.parent[q] = p
-        self.count -= 1
-        # self.nodeToNumOfChildren[q] doesn't matter anymore, because it's "the rank of non-findParent" node
-        self.nodeToNumOfChildren[p] += self.nodeToNumOfChildren[q]
-        # we don't delete the self.nodeToNumOfChildren[p] here, because we need all
-        # the intermediate self.parent[x]s to find the ultimate findParent, even
-        # though we did the path compression.
-
+print Solution().numIslands2(4, 4, [[0, 1], [0, 2], [1, 1], [1, 3], [2, 2], [3, 2], [1, 2]])
+print Solution().numIslands2(3, 3, [[0, 0], [0, 1], [1, 2], [2, 1]])
