840034787711f5d6eed1382e4032529b58e5b6ba
Hao Wu <how@amazon.com>
2019-11-02 23:52:07 -0700

    add changes

--- leetcode105 Construct Binary Tree from Preorder and Inorder Traversal.py
+++ leetcode105 Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -3,43 +3,51 @@
 
 class TreeNode(object):
     def __init__(self, x):
-
         self.val = x
         self.left = None
         self.right = None
 
 
-# reuse the solution for lc106.
+# One of classic binary tree questions. The idea is:
+    # 1) The preorder[0] is the root the recursion should return;
+    # 2) the inorder[inL:inR + 1] are the subtree under the root of this recursion
+    # 3) use recursion to build the subtree, then return the root of this recursion
 
 class Solution(object):
     def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
 
-        if not inorder: return
+        valueToIndex = {}
+        for i, value in enumerate(inorder):
+            valueToIndex[value] = i
 
-        self.inorder = inorder
-        self.preorder = preorder
-        self.position = {}
-        for i, num in enumerate(inorder):
-            self.position[num] = i
+        return self.buildSubTree(preorder, 0, inorder, 0, len(inorder) - 1, valueToIndex)
 
-        def helper(inlo, inhi, prelo, prehi):  # define the work range in list inorder and preorder
+    # preIndex points to the root this recursion should return
+    # inorder[inL:inR + 1] separates the nodes in this subtree
+    def buildSubTree(self, preorder, preIndex, inorder, inL, inR, valueToIndex):
+        if inL > inR:
+            return None
+        if inL == inR:
+            return TreeNode(inorder[inL])
 
-            if inlo > inhi:
-                return None
-            if inlo == inhi:
-                newnode = TreeNode(self.inorder[inlo])
-                return newnode
+        rootVal = preorder[preIndex]
+        root = TreeNode(rootVal)
 
-            parent = TreeNode(self.preorder[prelo])
-            mid = self.position[self.preorder[prelo]]
-            # pay attention to the boundary indices!!
-            # if any of them got messed up, it could exceed "maximum number of recursion"
-            lchild = helper(inlo, mid - 1, prelo + 1, prelo + mid - inlo)
-            rchild = helper(mid + 1, inhi, prehi + mid + 1 - inhi, prehi)
+        rootIndexInorder = valueToIndex[rootVal]
 
-            parent.left = lchild
-            parent.right = rchild
-            return parent
+        numOfNodesInLeft = rootIndexInorder - inL
 
-        root = helper(0, len(self.inorder) - 1, 0, len(self.preorder) - 1)
+        leftChild = self.buildSubTree(preorder, preIndex + 1, inorder, inL, rootIndexInorder - 1, valueToIndex)
+        root.left = leftChild
+
+        rightChild = self.buildSubTree(preorder, preIndex + 1 + numOfNodesInLeft, inorder, rootIndexInorder + 1, inR, valueToIndex)
+        root.right = rightChild
+
         return root
