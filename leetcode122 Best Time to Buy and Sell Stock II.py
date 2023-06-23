4bd1ca1bd3358ef38766a9a965961b65e450a304
whglamrock <haowu19930908@gmail.com>
2023-06-23 09:44:04 -0700

    add changes

--- leetcode122 Best Time to Buy and Sell Stock II.py
+++ leetcode122 Best Time to Buy and Sell Stock II.py
@@ -11,15 +11,14 @@ class Solution(object):
             return 0
 
         profit = 0
-        for i in xrange(len(prices) - 1):
+        for i in range(len(prices) - 1):
             if prices[i + 1] > prices[i]:
                 profit += prices[i + 1] - prices[i]
 
         return profit
 
 
-
-print Solution().maxProfit([1, 4, 5, 2, 9, 16])
-print Solution().maxProfit([7, 1, 5, 3, 6, 4])
-print Solution().maxProfit([1, 2, 3, 4, 5])
-print Solution().maxProfit([7, 6, 4, 3, 1])
+print(Solution().maxProfit([1, 4, 5, 2, 9, 16]))
+print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))
+print(Solution().maxProfit([1, 2, 3, 4, 5]))
+print(Solution().maxProfit([7, 6, 4, 3, 1]))
