e41c89fee15c52ec76653a7c43de20652fa90cf6
whglamrock <haowu19930908@gmail.com>
2024-01-24 11:04:29 -0800

    add changes

--- leetcode41 First Missing Positive.py
+++ leetcode41 First Missing Positive.py
@@ -1,34 +1,35 @@
+from typing import List
 
-# the hard part is using constant space
-
-class Solution(object):
-    def firstMissingPositive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if not nums:
-            return 1
-
+# shouldn't be too hard to think of moving each number to its corresponding index (e.g., 5 should be in nums[5])
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        # this guarantees that the nums has a 0, so that
+        # we can check if nums[i] == i instead of nums[i] == i - 1
         nums.append(0)
         n = len(nums)
 
-        # delete those useless numbers since they are not candidates
-        for i in xrange(n):
-            if nums[i] < 0 or nums[i] >= n:
+        for i, num in enumerate(nums):
+            if num < 0 or num >= n:
                 nums[i] = 0
 
-        # use index as the hash key to count each number's frequency
-        for i in xrange(n):
-            # nums[i] may have been "+n"'ed a lot of times, so "%n" is to get the original value
-            nums[nums[i] % n] += n
+        for i in range(n):
+            if nums[i] == 0:
+                continue
+            while nums[i] != i:
+                j = nums[i]
+                # to avoid infinite loop
+                if nums[j] == j:
+                    break
+                nums[i], nums[j] = nums[j], nums[i]
 
-        for i in xrange(1, n):
-            if nums[i] < n:
+        for i in range(1, n):
+            if nums[i] != i:
                 return i
 
         return n
 
 
-
-print Solution().firstMissingPositive([-2, 3, 7, 8, 9, 11, 12])
+print(Solution().firstMissingPositive([-2, 3, 7, 8, 9, 11, 12]))
+print(Solution().firstMissingPositive([0, 3]))
+# test case like this is why we append 0 at first
+print(Solution().firstMissingPositive([1]))
