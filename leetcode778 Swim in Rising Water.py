94fa31c103de8cc7d6e5fd22bea50079485a680b
whglamrock <haowu19930908@gmail.com>
2024-01-25 16:30:19 -0800

    add changes

--- leetcode778 Swim in Rising Water.py
+++ leetcode778 Swim in Rising Water.py
@@ -6,26 +6,21 @@ from typing import List
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        # when each (i, j) is visited, we store the smallest (highest elevation seen in the path)
-        indexToSmallestHighestInPath = {}
-        # use pq to always search from the lowest elevation
-        pq = [[grid[-1][-1], n - 1, n - 1]]
+        # stores the min (max elevation of the route progressed so far) of all routes passing through (i, j)
+        indexToMaxElevation = {(0, 0): grid[0][0]}
+        pq = [[grid[0][0], 0, 0]]
         directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-
         while pq:
-            currHighest, i, j = heappop(pq)
-            if (i, j) not in indexToSmallestHighestInPath or indexToSmallestHighestInPath[(i, j)] > currHighest:
-                indexToSmallestHighestInPath[(i, j)] = currHighest
-                for deltaI, deltaJ in directions:
-                    ii, jj = i + deltaI, j + deltaJ
-                    if 0 <= ii < n and 0 <= jj < n:
-                        if (ii, jj) == (0, 0):
-                            return max(currHighest, grid[0][0])
-                        # only add it to heapq if the next index is not visited or has a bigger smallest elevation in path
-                        if (ii, jj) not in indexToSmallestHighestInPath or indexToSmallestHighestInPath[(ii, jj)] > currHighest:
-                            heappush(pq, [max(grid[ii][jj], currHighest), ii, jj])
+            currHeight, i, j = heappop(pq)
+            if i == j == n - 1:
+                return indexToMaxElevation[(i, j)]
+            for deltaI, deltaJ in directions:
+                ii, jj = i + deltaI, j + deltaJ
+                if 0 <= ii < n and 0 <= jj < n:
+                    newHeight = max(currHeight, grid[ii][jj])
+                    if (ii, jj) not in indexToMaxElevation or indexToMaxElevation[(ii, jj)] > newHeight:
+                        indexToMaxElevation[(ii, jj)] = newHeight
+                        heappush(pq, [newHeight, ii, jj])
 
-        return indexToSmallestHighestInPath[(0, 0)]
-
-
-
+        # useless, just to avoid syntax warning
+        return 0
--- leetcode827 Making A Large Island.py
+++ leetcode827 Making A Large Island.py
@@ -1,74 +1,52 @@
-from copy import deepcopy
+from typing import List, Dict
 
-
-class Solution(object):
-    def largestIsland(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        indexToArea = {}
-        n = len(grid)
-        # copy the grid to do dfs ("sink the island" idea in the classic Number of Islands problem)
-        grid_copy = deepcopy(grid)
-        connectedIndexes = {}
-
-        # the islandId records which island each index belongs to
+class Solution:
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        indexToIsland = {}
         islandId = 0
-        ans = 0
-        for i in range(n):
-            for j in range(n):
-                sunkIndexes = set()
-                # all connected 1's will become 0 so islandId will always be accurate
-                if grid_copy[i][j] == 1:
-                    self.dfs(i, j, grid_copy, sunkIndexes)
-                    for index in sunkIndexes:
-                        indexToArea[index] = len(sunkIndexes)
-                        connectedIndexes[index] = islandId
-                    islandId += 1
-                    ans = max(ans, len(sunkIndexes))
-
-        for i in range(n):
-            for j in range(n):
+        islandToArea = {}
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == 1:
+                    area = self.sinkTheIsland(grid, i, j, indexToIsland, islandId)
+                    islandToArea[islandId] = area
+                    islandId += 1
+
+        # even if it's all 0 we can flip one 0 to 1.
+        maxConnectedIsland = max(islandToArea.values()) if islandToArea else 1
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i, j) in indexToIsland:
                     continue
-                areaConnected = 0
-                # usedIslands is to dedupe the island ID being used
-                usedIslands = set()
-                if i - 1 >= 0 and grid[i - 1][j] == 1:
-                    areaConnected += indexToArea[(i - 1, j)]
-                    usedIslands.add(connectedIndexes[(i - 1, j)])
-                if i + 1 < n and grid[i + 1][j] == 1:
-                    if connectedIndexes[(i + 1, j)] not in usedIslands:
-                        areaConnected += indexToArea[(i + 1, j)]
-                        usedIslands.add(connectedIndexes[(i + 1, j)])
-                if j - 1 >= 0 and grid[i][j - 1] == 1:
-                    if connectedIndexes[(i, j - 1)] not in usedIslands:
-                        areaConnected += indexToArea[(i, j - 1)]
-                        usedIslands.add(connectedIndexes[(i, j - 1)])
-                if j + 1 < n and grid[i][j + 1] == 1:
-                    if connectedIndexes[(i, j + 1)] not in usedIslands:
-                        areaConnected += indexToArea[(i, j + 1)]
-                        usedIslands.add(connectedIndexes[(i, j + 1)])
-                ans = max(ans, areaConnected + 1)
+                neighborIslands = set()
+                if i - 1 >= 0 and (i - 1, j) in indexToIsland:
+                    neighborIslands.add(indexToIsland[(i - 1, j)])
+                if i + 1 < len(grid) and (i + 1, j) in indexToIsland:
+                    neighborIslands.add(indexToIsland[(i + 1, j)])
+                if j - 1 >= 0 and (i, j - 1) in indexToIsland:
+                    neighborIslands.add(indexToIsland[(i, j - 1)])
+                if j + 1 < len(grid[0]) and (i, j + 1) in indexToIsland:
+                    neighborIslands.add(indexToIsland[(i, j + 1)])
 
-        return ans
+                connectedArea = 0
+                for neighborIsland in neighborIslands:
+                    connectedArea += islandToArea[neighborIsland]
+                if connectedArea:
+                    maxConnectedIsland = max(maxConnectedIsland, connectedArea + 1)
 
-    # sink the island and record the indexes
-    def dfs(self, i, j, grid, sunkIndexes):
-        if grid[i][j] == 0:
-            return
-        if grid[i][j] == 1:
-            grid[i][j] = 0
-            sunkIndexes.add((i, j))
-        if i - 1 >= 0:
-            self.dfs(i - 1, j, grid, sunkIndexes)
-        if i + 1 < len(grid):
-            self.dfs(i + 1, j, grid, sunkIndexes)
-        if j - 1 >= 0:
-            self.dfs(i, j - 1, grid, sunkIndexes)
-        if j + 1 < len(grid[0]):
-            self.dfs(i, j + 1, grid, sunkIndexes)
+        return maxConnectedIsland
+
+    def sinkTheIsland(self, grid: List[List[int]], i: int, j: int, indexToIsland: Dict[tuple, int], islandId: int):
+        grid[i][j] = 0
+        indexToIsland[(i, j)] = islandId
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        area = 1
+        for deltaI, deltaJ in directions:
+            ii, jj = i + deltaI, j + deltaJ
+            if 0 <= ii < len(grid) and 0 <= jj < len(grid[0]) and grid[ii][jj] == 1:
+                area += self.sinkTheIsland(grid, ii, jj, indexToIsland, islandId)
+
+        return area
 
 
 grid1 = [[1, 0, 1],
