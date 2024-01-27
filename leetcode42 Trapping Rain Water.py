6c3d912c570e143b991d4a979ba680967c845b0d
whglamrock <haowu19930908@gmail.com>
2024-01-27 12:37:23 -0800

    add changes

--- leetcode42 Trapping Rain Water.py
+++ leetcode42 Trapping Rain Water.py
@@ -5,7 +5,8 @@ class Solution:
         stack = []
         area = 0
         for i, h in enumerate(height):
-            # when prevH == h we still pop because the area will increment 0
+            # when prevH == h we still need to pop.
+            # e.g., consider edge case like [5, 2, 1, 2, 1, 5]
             while stack and stack[-1][1] <= h:
                 prevI, prevH = stack.pop()
                 if stack:
@@ -18,3 +19,5 @@ class Solution:
 print(Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
 print(Solution().trap([4, 2, 0, 3, 2, 5]))
 print(Solution().trap([0, 1, 0, 3, 1, 0, 3, 4, 2, 1, 3, 1]))
+# if we use while stack and stack[-1][1] < h, below will yield wrong answer 16
+print(Solution().trap([5, 2, 1, 2, 1, 5]))
