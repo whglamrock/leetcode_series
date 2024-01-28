58db57c925c8d9c5ebac3d09f48c9e9af1a561e0
whglamrock <haowu19930908@gmail.com>
2024-01-28 01:08:51 -0800

    add changes

--- leetcode115 Distinct Subsequences.py
+++ leetcode115 Distinct Subsequences.py
@@ -1,33 +1,42 @@
-
-# Time complexity will have to be at least O(mn), but space complexity can be improved to O(n).
-# However, in real interview, getting out the below solution is very enough
-
-class Solution(object):
-    def numDistinct(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: int
-        """
-
+# The stupid leetcode doesn't accept dfs + memoization solution, only DP works in LC.
+# But the cached dfs should work in real interview. See dfs approach at the bottom.
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
         m, n = len(s), len(t)
-        # dp[i][j] means the total number of ways for s[:i] and t[:j] to match
-        dp = [[0 for j in xrange(n + 1)] for i in xrange(m + 1)]
-        # do not also set dp[0][j] here because it's s that needs to remove chars, not t
-        for i in xrange(m + 1):
+        # dp[i][j] means the number of subsequences of s[:i] to match t[:j]
+        dp = [[0 for j in range(n + 1)] for i in range(m + 1)]
+        for i in range(m + 1):
             dp[i][0] = 1
 
-        for i in xrange(1, m + 1):
-            for j in xrange(1, n + 1):
-                # we can simply populate the matching status of (s[:i - 1] & t[:j]) by removing s[i - 1]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                # not using s[i - 1], see if any subsequence generate t[:j]
                 dp[i][j] = dp[i - 1][j]
-                # normal case
+                # use s[i - 1] to match t[j - 1]
                 if s[i - 1] == t[j - 1]:
                     dp[i][j] += dp[i - 1][j - 1]
 
         return dp[-1][-1]
 
 
+print(Solution().numDistinct("babagbag", "bag"))
+print(Solution().numDistinct("rabbbit", "rabbit"))
 
-print Solution().numDistinct("babagbag", "bag")
-print Solution().numDistinct("rabbbit", "rabbit")
+
+'''
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        return self.dfs(s, t, 0, 0)
+    
+    @lru_cache(None)
+    def dfs(self, s: str, t: str, i: int, j: int) -> int:
+        if j == len(t):
+            return 1
+        
+        numOfWays = 0
+        for ii in range(i, len(s)):
+            if s[ii] == t[j]:
+                numOfWays += self.dfs(s, t, ii + 1, j + 1)
+        
+        return numOfWays
+'''
