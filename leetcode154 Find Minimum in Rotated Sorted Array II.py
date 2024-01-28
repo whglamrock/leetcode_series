87c2364361385a61c182c79f1a5b1ba6ac4b8bbc
whglamrock <haowu19930908@gmail.com>
2024-01-28 15:50:37 -0800

    add changes

--- leetcode154 Find Minimum in Rotated Sorted Array II.py
+++ leetcode154 Find Minimum in Rotated Sorted Array II.py
@@ -1,52 +1,45 @@
+from typing import List
 
-class Solution(object):
-    def findMin(self, nums):
-
-        if (not nums) or len(nums) == 0:
-            return
-
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
-        while l < r:
-            mid = l + (r - l) / 2
-            if nums[mid] > nums[r]:  # nums[mid] can't be the answer
-                l = mid + 1
-            elif nums[mid] < nums[r]:  # nums[mid] could be the answer
-                r = mid
+        while l <= r:
+            m = (l + r) // 2
+            if l == r:
+                return nums[m]
+            # not rotated
+            if nums[l] < nums[r]:
+                return nums[l]
+            # rotated
+            elif nums[l] > nums[r]:
+                if nums[m] <= nums[r]:
+                    r = m
+                else:
+                    l = m + 1
+            # nums[l] == nums[r] could be rotated or not
+            # if it's not rotated, there is only 1 possibility where all nums[i] are equal
             else:
-                r -= 1
+                if nums[m] < nums[r]:
+                    r = m
+                elif nums[m] > nums[r]:
+                    l = m + 1
+                else:
+                    return self.moveLeftOrRightToFindMin(nums, m)
 
-        return nums[l]  # return nums[r] is the same.
+    def moveLeftOrRightToFindMin(self, nums: List[int], i: int) -> int:
+        minValue = nums[i]
+        for j in range(i + 1, len(nums)):
+            if nums[j] > minValue:
+                break
+            minValue = min(minValue, nums[j])
+        for j in range(i - 1, -1, -1):
+            if nums[j] > minValue:
+                break
+            minValue = min(minValue, nums[j])
+        return minValue
 
-# extreme cases like [10, 1, 10, 10, 10] and [10, 10, 10, 1, 10] are very
-# important
 
-
-
-'''
-# the following code has problem for test case like [10, 1, 10, 10, 10]:
-class Solution(object):
-    def findMin(self, nums):
-
-        if (not nums) or len(nums) == 0:
-            return
-
-        l, r = 0, len(nums) - 1
-        while l < r:
-            #print l, r
-            mid = l + (r - l) / 2
-            if nums[mid] > nums[l]:
-                l = mid + 1
-            elif nums[mid] < nums[l]:
-                r = mid
-            else:
-                l += 1
-
-        return nums[l]
-
-# because for "nums[mid] > nums[r]" we can absolutely be sure that we need to
-# move left. But for "nums[mid] > nums[l]" we can't. For example, in these two cases:
-# [4,5,6,7,0,1,2] and [1, 10, 10, 10], both satisfy nums[mid] > nums[l], but we could
-# move to mid's left or right. But nums[mid] > nums[r] doesn't have this problem, for
-# [4,5,6,7,0,1,2] and [10, 10, 10, 1], both move to the right.
-
-'''
+print(Solution().findMin([4, 4, 4, 4, 4, 4, 4, 0, 1, 1, 3, 4, 4]))
+print(Solution().findMin([4, 4, 4, 4, 0, 1, 1, 3, 4, 4, 4, 4]))
+print(Solution().findMin([4, 4, 4, 4, 0, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]))
+print(Solution().findMin([4, 4, 4, 4, 4, 4, 4, 4, 4, 4]))
