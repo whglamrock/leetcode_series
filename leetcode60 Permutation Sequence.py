79743d4db2df4db3cbdf1a30c58d9de138231787
whglamrock <haowu19930908@gmail.com>
2024-01-27 17:15:34 -0800

    add changes

--- leetcode60 Permutation Sequence.py
+++ leetcode60 Permutation Sequence.py
@@ -1,35 +1,19 @@
+from math import ceil, factorial
 
-class Solution(object):
-    def getPermutation(self, n, k):
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        nFactorial = factorial(n)
+        ans = []
+        nums = [i for i in range(1, n + 1)]
+        for i in range(n, 0, -1):
+            divider = nFactorial // i
+            nFactorial = divider
+            digit = nums[ceil(k / divider) - 1]
+            ans.append(str(digit))
+            nums.remove(digit)
+            k %= divider
 
-        jiecheng = 1
-        for i in xrange(1,n+1):
-            jiecheng *= i
-
-        divider = n
-        base = []
-        used = set([])
-        while divider > 0:
-            interval = jiecheng/divider
-            cur, counter = 0, 0
-            while counter * interval < k:
-                cur += 1
-                counter += 1    # used to count the number of interval it's currently in
-                while cur in used:  # in permutation sequence, every number(1~n) can only be used once
-                    cur += 1
-            base.append(str(cur))    # stores the number we've chosen
-            used.update({cur})
-            k -= (counter-1) * interval    # the k left over
-            if k == 0:
-                break
-            jiecheng /= divider    # this two lines are designed to change the interval in next loop
-            divider -= 1
-
-        return ''.join(base)
+        return ''.join(ans)
 
 
-
-Sol = Solution()
-n = 4
-k = 2
-print Sol.getPermutation(n, k)
+print(Solution().getPermutation(9, 10000))
--- leetcode65 Valid Number.py
+++ leetcode65 Valid Number.py
@@ -1,64 +1,36 @@
-
-# the DFA state transition table is the most elegant solution for this. I don't expect myself to really remember
-# this algorithm. Once the table is constructed, I should complete coding part easily.
-
-class Solution(object):
-    def isNumber(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        #define DFA state transition tables
-        states = [{},
-
-                 # State (1) - initial state (scan ahead thru blanks)
-                 {'blank': 1, 'sign': 2, 'digit': 3, '.': 4},
-
-                 # State (2) - found sign (expect digit/dot). e.g. +.2, -2, etc.
-                 {'digit': 3, '.': 4},
-
-                 # State (3) - digit consumer (loop until non-digit). P.S. the value for '.' has to be 5 instead of 4 because
-                   # '+.' is different from '5.'. '5.' is a valid ending state
-                 {'digit': 3, '.': 5, 'e': 6, 'blank': 9}, # state 3 means so far we only have integer. e.g., +333, 012314
-
-                 # State (4) - found dot (only a digit is valid)
-                 {'digit': 5},
-
-                 # State (5) - after dot (expect digits, e, or end of valid input)
-                 {'digit': 5, 'e': 6, 'blank': 9},  # P.S. +5., 4. are valid inputs!
-
-                 # State (6) - found 'e' (only a sign or digit valid)
-                 {'sign': 7, 'digit': 8},
-
-                 # State (7) - sign after 'e' (only digit)
-                 {'digit': 8},
-
-                 # State (8) - digit after 'e' (expect digits or end of valid input)
-                 {'digit': 8, 'blank': 9},
-
-                 # State (9) - Terminal state (fail if non-blank found afterwards)
-                 {'blank': 9}]
-
-        currentState = 1
-        for c in s:
-            # If char c is of a known class set it to the class name
-            if c in '0123456789':
-                c = 'digit'
-            elif c in ' \t\n':
-                c = 'blank'
-            elif c in '+-':
-                c = 'sign'
-            # elif c is e, c = e, else return False
-
-            # If char/class is not in our state transition table it is invalid input, this condition
-              # rules out the case when c == e
-            if c not in states[currentState]:
-                return False
-            # State transition
-            currentState = states[currentState][c]
-
-        # The only valid terminal states are end on digit, after dot, digit after e, or white space after valid input
-        # also, we cannot include state 4 as valid because simply something like '+.', '.' are not valid
-        if currentState not in [3, 5, 8, 9]:
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        eCount = 0
+        digitCount = 0
+        splitter = ''
+        for char in s:
+            if char in 'eE':
+                eCount += 1
+                splitter = char
+            if char.isdigit():
+                digitCount += 1
+        if eCount > 1 or digitCount == 0:
             return False
-        return True
+        if eCount == 0:
+            return self.isValidNumber(s)
+
+        leftNum, rightNum = s.split(splitter)[0], s.split(splitter)[1]
+        return self.isValidNumber(leftNum) and '.' not in rightNum and self.isValidNumber(rightNum)
+
+    def isValidNumber(self, s: str) -> bool:
+        if not s:
+            return False
+        if s[0] in '+-':
+            s = s[1:]
+
+        dotCount = 0
+        digitCount = 0
+        for char in s:
+            if char == '.':
+                dotCount += 1
+            elif char.isdigit():
+                digitCount += 1
+            else:
+                return False
+
+        return dotCount <= 1 and digitCount > 0
