e6b3088127f649ce00eb4c1db06594c65c0a436d
whglamrock <haowu19930908@gmail.com>
2024-01-29 21:44:14 -0800

    add changes

--- leetcode239 Sliding Window Maximum.py
+++ leetcode239 Sliding Window Maximum.py
@@ -1,25 +1,19 @@
-
 from collections import deque
+from typing import List
 
 # O(N) decreasing queue/stack solution
-class Solution(object):
-    def maxSlidingWindow(self, nums, k):
-
-        window = deque()
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        stack = deque()
         ans = []
-
         for i, num in enumerate(nums):
-            # remove the elements out of the window
-            while window and window[0] < i - k + 1:
-                window.popleft()
-            # the window is a decreasing queue because we don't care about the smaller elements in between
-            while window and nums[window[-1]] <= num:
-                window.pop()
-            # add the current index to window
-            window.append(i)
-            # add max of window to ans
+            while stack and i - stack[0][0] + 1 > k:
+                stack.popleft()
+            while stack and stack[-1][1] <= num:
+                stack.pop()
+            stack.append([i, num])
             if i >= k - 1:
-                ans.append(nums[window[0]])
+                ans.append(stack[0][1])
 
         return ans
 
--- leetcode265 Paint House II.py
+++ leetcode265 Paint House II.py
@@ -1,49 +1,42 @@
+from collections import deque
+from typing import List
 
-class Solution(object):
-    def minCostII(self, costs):
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+        n, k = len(costs), len(costs[0])
+        # dp[i][j] means the min cost of painting i houses and the ith house is painted with color j
+        dp = [[2147483647 for j in range(k)] for i in range(n + 1)]
+        for j in range(k):
+            dp[0][j] = 0
 
-        # invalid null case
-        if not costs or not costs[0]:
-            return 0
-
-        # when there is only one available color
-        if len(costs[0]) == 1:
-            if len(costs) > 1:
-                return 2147483647   # or whatever other value
-            else:
-                return costs[0][0]
-
-        n = len(costs)
-        dp = [[0 for j in xrange(len(costs[0]))] for i in xrange(n)]
-        dp[0] = costs[0]
-
-        for i in xrange(1, n):
-            # find the minvalue from previous dp[i]
-            prevmin = min(dp[i - 1])
-
-            # find all possible previous paint colors that can reach the previous minvalue
-            prevminset = set()
-            for j in xrange(len(costs[0])):
-                if dp[i - 1][j] == prevmin:
-                    prevminset.add(j)
-
-            # find the second min value, in case that the current color
-            #   is same as the color that reaches the previous minvalue
-            prevsecondmin = 2147483647
-            for j in xrange(len(costs[0])):
-                if dp[i - 1][j] == prevmin:
-                    continue
-                if dp[i - 1][j] < prevsecondmin:
-                    prevsecondmin = dp[i - 1][j]
-
-            for j in xrange(len(costs[0])):
-                if j not in prevminset:
-                    dp[i][j] = prevmin + costs[i][j]
+        minCostFromLeft, minCostFromRight = [], deque()
+        for i in range(1, n + 1):
+            currMin = 2147483647
+            nextMinCostFromLeft, nextMinCostFromRight = [], deque()
+            for j in range(k):
+                if not minCostFromLeft:
+                    dp[i][j] = dp[i - 1][j] + costs[i - 1][j]
                 else:
-                    # means we have mutiple conbinations that reaches the previous minvalue
-                    if len(prevminset) > 1:
-                        dp[i][j] = prevmin + costs[i][j]
-                    else:
-                        dp[i][j] = prevsecondmin + costs[i][j]
+                    if j - 1 >= 0:
+                        dp[i][j] = min(dp[i][j], minCostFromLeft[j - 1] + costs[i - 1][j])
+                    if j + 1 < k:
+                        dp[i][j] = min(dp[i][j], minCostFromRight[j + 1] + costs[i - 1][j])
+                currMin = min(currMin, dp[i][j])
+                nextMinCostFromLeft.append(currMin)
+
+            # scanning from right to left
+            currMin = 2147483647
+            for j in range(k - 1, -1, -1):
+                currMin = min(currMin, dp[i][j])
+                nextMinCostFromRight.appendleft(currMin)
+
+            minCostFromLeft, minCostFromRight = nextMinCostFromLeft, nextMinCostFromRight
 
         return min(dp[-1])
+
+
+print(Solution().minCostII([[1, 5, 3], [2, 9, 4]]))
+print(Solution().minCostII([[1, 3], [2, 4]]))
+print(Solution().minCostII([
+    [15, 17, 15, 20, 7, 16, 6, 10, 4, 20, 7, 3, 4],
+    [11, 3, 9, 13, 7, 12, 6, 7, 5, 1, 7, 18, 9]]))
