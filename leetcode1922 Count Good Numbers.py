1735e39526a315e24d982882aefb33620a1080a3
whglamrock <haowu19930908@gmail.com>
2024-02-11 17:44:51 -0800

    add changes

--- leetcode1922 Count Good Numbers.py
+++ leetcode1922 Count Good Numbers.py
@@ -1,5 +1,14 @@
 
-# what's the fucking point of such question?
+# If below solution gets TLE, what's the fucking point of such question? motherfucking stupid leetcode.
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        numOfEvenDigits = (n + 1) // 2
+        numOfOddDigits = n - numOfEvenDigits
+        return (pow(5, numOfEvenDigits) * pow(4, numOfOddDigits)) % (10 ** 9 + 7)
+
+
+'''
+# The only python solution that won't get TLE in the stupid leetcode 
 # idea from: https://leetcode.com/problems/count-good-numbers/solutions/1314363/java-python-3-iterative-o-logn-code-similar-to-lc50-pow-x-n-w-brief-explanation-and-analysis/
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
@@ -12,3 +21,4 @@ class Solution:
 
         MOD = 10 ** 9 + 7
         return 5 ** (n % 2) * countGood(n // 2, 4 * 5) % MOD
+'''
