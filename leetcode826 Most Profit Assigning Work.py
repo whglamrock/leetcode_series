b43fb15c511df03969ebab0c593824e534fc1a2b
whglamrock <haowu19930908@gmail.com>
2024-02-13 22:38:26 -0800

    add changes

--- leetcode826 Most Profit Assigning Work.py
+++ leetcode826 Most Profit Assigning Work.py
@@ -3,20 +3,16 @@ from typing import List
 
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        workersWithIndex = []
-        for i in range(len(worker)):
-            workersWithIndex.append([worker[i], i])
-        workersWithIndex.sort(key=lambda x: -x[0])
-
-        pq = []
+        worker.sort(reverse=True)
+        jobs = []
         for i in range(len(difficulty)):
-            heappush(pq, [-profit[i], difficulty[i]])
+            heappush(jobs, [-profit[i], difficulty[i]])
 
-        ans = 0
-        for ability, i in workersWithIndex:
-            while pq and pq[0][1] > ability:
-                heappop(pq)
-            if pq:
-                ans += -pq[0][0]
+        maxProfit = 0
+        for capability in worker:
+            while jobs and jobs[0][1] > capability:
+                heappop(jobs)
+            if jobs:
+                maxProfit += -jobs[0][0]
 
-        return ans
+        return maxProfit
