ad5f28b91ca801a925e952b0f29444825f7c1a79
whglamrock <haowu19930908@gmail.com>
2024-02-14 15:17:25 -0800

    add changes

--- leetcode37 Sudoku Solver.py
+++ leetcode37 Sudoku Solver.py
@@ -1,17 +1,16 @@
+from typing import List
 
 # Backtracking. The time complexity is as high as O(9^N), where N is the number of cells to fill.
-
-class Solution(object):
-    def solveSudoku(self, board):
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
         """
-        :type board: List[List[str]]
-        :rtype: None Do not return anything, modify board in-place instead.
+        Do not return anything, modify board in-place instead.
         """
         self.backtracking(board)
 
-    def backtracking(self, board):
-        for i in xrange(9):
-            for j in xrange(9):
+    def backtracking(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            for j in range(9):
                 if board[i][j] != '.':
                     continue
                 for num in '123456789':
@@ -23,12 +22,12 @@ class Solution(object):
                 return False
         return True
 
-    def isValid(self, board, i, j, num):
-        subBox_i = i / 3
-        subBox_j = j / 3
-        for x in xrange(9):
+    def isValid(self, board: List[List[str]], i: int, j: int, num: str) -> bool:
+        subBoxI = i // 3
+        subBoxJ = j // 3
+        for x in range(9):
             if board[i][x] == num or board[x][j] == num:
                 return False
-            if board[subBox_i * 3 + x / 3][subBox_j * 3 + x % 3] == num:
+            if board[subBoxI * 3 + x // 3][subBoxJ * 3 + x % 3] == num:
                 return False
         return True
