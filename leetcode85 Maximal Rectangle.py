6cb48f81ebaebd01f099e8d7d957611246c170bb
whglamrock <haowu19930908@gmail.com>
2024-02-18 14:27:01 -0800

    add changes

--- leetcode85 Maximal Rectangle.py
+++ leetcode85 Maximal Rectangle.py
@@ -1,69 +1,40 @@
+from typing import List
 
-# a typical multi-dpArray dp question. In this question, we could also use 2-D array for left, right, height but
-    # 1-D takes less space
-# explanation from: https://discuss.leetcode.com/topic/6650/share-my-dp-solution
-
-class Solution(object):
-    def maximalRectangle(self, matrix):
-        """
-        :type matrix: List[List[str]]
-        :rtype: int
-        """
-        if not matrix or not matrix[0]:
-            return 0
-
+# The idea comes from lc84: Largest Rectangle in Histogram.
+# 1) We consider consecutive 1's in each row as "pillar" to form part of the rectangle
+# 2) Keep a heights array when we loop through each row. If any element is 0 the height of the pillar becomes 0
+# 3) Then within each row, we have a heights aray, this becomes the Largest Rectangle in Histogram problem
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
         m, n = len(matrix), len(matrix[0])
-        # left[j] stores the left most index k that all left[k:j + 1] is 1
-        left = [0] * n
-        # right[j] stores the right most index k that all right[j:k + 1] is 1
-        right = [n] * n
-        height = [0] * n
-
+        heights = [0] * n
         maxArea = 0
-        for i in xrange(m):
-            currLeft, currRight = 0, n
-            for j in xrange(n):
-                if matrix[i][j] == '1':
-                    # 1. not making left[j] directly = currLeft because currLeft only means the leftmost consecutive 1
-                        # and the corresponding height[currLeft] is very possibly different
-                    # 2. the previous left[j] represents that with the previous height[i], how left we can go
-                    # 3. the currLeft only records for the current row
-                    left[j] = max(left[j], currLeft)
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '0':
+                    heights[j] = 0
                 else:
-                    currLeft = j + 1
-                    # reset it 0 is for the next big for loop (i + 1)
-                    # this won't affect the area calculation because the corresponding
-                    # height[j] will be 0
-                    left[j] = 0
+                    heights[j] += 1
 
-            for j in xrange(n - 1, -1, -1):
-                if matrix[i][j] == '1':
-                    right[j] = min(right[j], currRight)
-                else:
-                    # notice that we make currRight = j instead of j - 1 to ease the area calculation
-                    currRight = j
-                    # reset it n is for the next big for loop (i + 1)
-                    right[j] = n
-
-            for j in xrange(n):
-                if matrix[i][j] == '1':
-                    height[j] += 1
-                else:
-                    height[j] = 0
-
-            for j in xrange(n):
-                # no need to do right[j] - left[j] "+ 1" because right[j] stores the index + 1
-                maxArea = max(maxArea, (right[j] - left[j]) * height[j])
+            # increasing stack
+            stack = [[-1, 0]]
+            for j, h in enumerate(heights):
+                while stack and h < stack[-1][1]:
+                    prevIndex, prevH = stack.pop()
+                    maxArea = max(maxArea, prevH * (j - stack[-1][0] - 1))
+                stack.append([j, h])
+            while stack:
+                prevIndex, prevH = stack.pop()
+                if stack:
+                    maxArea = max(maxArea, prevH * (n - stack[-1][0] - 1))
 
         return maxArea
 
 
-
 matrix = [
-  ["1","0","1","0","0"],
-  ["1","0","1","1","1"],
-  ["1","1","1","1","1"],
-  ["1","0","0","1","0"]
+    ["1", "0", "1", "0", "0"],
+    ["1", "0", "1", "1", "1"],
+    ["1", "1", "1", "1", "1"],
+    ["1", "0", "0", "1", "0"]
 ]
-print Solution().maximalRectangle(matrix)
-
+print(Solution().maximalRectangle(matrix))
