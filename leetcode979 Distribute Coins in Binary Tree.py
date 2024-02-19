9054462bbd8503290112f83ed1187a17981e8838
whglamrock <haowu19930908@gmail.com>
2024-02-18 17:49:29 -0800

    add changes

--- leetcode979 Distribute Coins in Binary Tree.py
+++ leetcode979 Distribute Coins in Binary Tree.py
@@ -1,37 +1,27 @@
+from typing import Optional
 
-# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-
-
-# idea from: https://leetcode.com/problems/distribute-coins-in-binary-tree/solution/, notice the intuition part
-
-# we count the number of moves needed for each NODE, and we use DFS from bottom to top to return the state after
-# each subtree has completed moves.
-
-class Solution(object):
-    def distributeCoins(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+class Solution:
+    def __init__(self):
         self.ans = 0
 
-        def dfs(node):
-
-            if not node:
-                return 0
-
-            L, R = dfs(node.left), dfs(node.right)
-            self.ans += abs(L) + abs(R)
-
-            # the L & R can offset each other, so we we count the number of moves
-            return (node.val - 1) + L + R
-
-        dfs(root)
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+        self.dfs(root)
         return self.ans
+
+    # returns the number of coins given to its parent
+    def dfs(self, node: Optional[TreeNode]) -> int:
+        if not node:
+            return 0
+        numOfCoinsFromLeft = self.dfs(node.left)
+        numOfCoinsFromRight = self.dfs(node.right)
+        # only count the move when the coins are actually moved to the node's parent
+        self.ans += abs(numOfCoinsFromLeft) + abs(numOfCoinsFromRight)
+        # node.val - 1 is the number of coins given to its parent from this node itself
+        return (node.val - 1) + numOfCoinsFromLeft + numOfCoinsFromRight
