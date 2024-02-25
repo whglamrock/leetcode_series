a51eab898745163109312d1a7c91dbc6598fcdab
whglamrock <haowu19930908@gmail.com>
2024-02-24 17:46:21 -0800

    add changes

---
+++ leetcode2444 Count Subarrays With Fixed Bounds.py
@@ -0,0 +1,28 @@
+from typing import List
+
+# Intuition:
+# 1) We keep a window where all elements in it are within [minK, maxK].
+# 2) We record the lastIndex of minK and maxK. If any element is out of boundary it means the left bound has to be reset
+# to current index + 1 (next index)
+# 3) If the element is within the bound, we keep expanding the window. If the window is valid, the current element can be
+# used as the right bound of the subarray. And the left bound of subarray can anywhere between
+# [l, min(lastMinKIndex, lastMaxKIndex)], inclusive.
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        lastMinKIndex, lastMaxKIndex = None, None
+        l = 0
+        ans = 0
+        for r, num in enumerate(nums):
+            if not minK <= num <= maxK:
+                lastMinKIndex, lastMaxKIndex = None, None
+                l = r + 1
+                continue
+            if num == minK:
+                lastMinKIndex = r
+            if num == maxK:
+                lastMaxKIndex = r
+
+            if lastMinKIndex is not None and lastMaxKIndex is not None:
+                ans += min(lastMinKIndex, lastMaxKIndex) - l + 1
+
+        return ans
