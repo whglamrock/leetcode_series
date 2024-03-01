94bdb6491a8368ad783370a15b37fe19a98d0665
whglamrock <haowu19930908@gmail.com>
2024-03-01 14:47:54 -0800

    add changes

--- leetcode199 Binary Tree Right Side View.py
+++ leetcode199 Binary Tree Right Side View.py
@@ -1,29 +1,27 @@
+from typing import Optional, List
 
-# Traversal of tree level by level and return the last node's value on each level
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-class TreeNode(object):
-    def __init__(self, x):
-
-        self.val = x
-        self.left = None
-        self.right = None
-
-
-class Solution(object):
-    def rightSideView(self, root):
-
-        if (not root):
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
             return []
-        stack = [root]
+
         ans = []
-        while stack:
-            new = []
-            ans.append(stack[-1].val)
-            for item in stack:
-                if item.left:
-                    new.append(item.left)
-                if item.right:
-                    new.append(item.right)
-            stack = new
+        # level order traversal
+        todo = [root]
+        while todo:
+            nextTodo = []
+            ans.append(todo[-1].val)
+            for node in todo:
+                if node.left:
+                    nextTodo.append(node.left)
+                if node.right:
+                    nextTodo.append(node.right)
+            todo = nextTodo
 
         return ans
--- leetcode270 Closest Binary Search Tree.py
+++ leetcode270 Closest Binary Search Tree.py
@@ -1,53 +1,30 @@
+from typing import Optional
 
-# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def __init__(self):
+        self.minDiff = 0
+        self.closestVal = 0
 
-class TreeNode(object):
-    def __init__(self, x):
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        self.closestVal = None
+        self.minDiff = 10 ** 10 + 1
+        self.dfs(root, target)
+        return self.closestVal
 
-        self.val = x
-        self.left = None
-        self.right = None
+    def dfs(self, node: TreeNode, target: float):
+        if abs(node.val - target) < self.minDiff:
+            self.minDiff = abs(node.val - target)
+            self.closestVal = node.val
+        elif abs(node.val - target) == self.minDiff:
+            if self.closestVal is None or self.closestVal > node.val:
+                self.closestVal = node.val
 
-
-class Solution(object):
-    def closestValue(self, root, target):
-
-        self.ans = root.val
-
-        if (not root):
-            return
-
-        def fku(node, target):
-            if (not node):
-                return
-            if abs(node.val - target) < abs(self.ans - target):
-                self.ans = node.val
-            fku(node.left, target)
-            fku(node.right, target)
-
-        fku(root, target)
-
-        return self.ans
-
-
-
-a = TreeNode(1)
-b = TreeNode(2)
-c = TreeNode(3)
-d = TreeNode(4)
-e = TreeNode(5)
-f = TreeNode(6)
-g = TreeNode(7)
-h = TreeNode(10)
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.left = f
-c.right = g
-d.left = h
-
-Sol = Solution()
-print Sol.closestValue(a, 8)
-
-
+        if node.left:
+            self.dfs(node.left, target)
+        if node.right:
+            self.dfs(node.right, target)
