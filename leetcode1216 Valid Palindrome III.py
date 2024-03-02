f88e9c90ec5824987128f389ad3fc883d6b4e77f
whglamrock <haowu19930908@gmail.com>
2024-03-01 16:34:44 -0800

    add changes

--- leetcode1216 Valid Palindrome III.py
+++ leetcode1216 Valid Palindrome III.py
@@ -1,32 +1,24 @@
+class Solution:
+    def isValidPalindrome(self, s: str, k: int) -> bool:
+        lenOfLongestPalindromeSubsqns = self.longestPalindromeSubsequence(s)
+        return len(s) - lenOfLongestPalindromeSubsqns <= k
 
-class Solution(object):
-    def isValidPalindrome(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: bool
-        """
-        lenOfLongestPalindromeSubsequence = self.longestPalindromeSubsequence(s)
-        return len(s) - lenOfLongestPalindromeSubsequence <= k
-
-    def longestPalindromeSubsequence(self, s):
+    def longestPalindromeSubsequence(self, s: str) -> int:
         n = len(s)
+        # dp[i][j] means the length longest palindrome subsequence within s[i:j + 1]
         dp = [[0 for j in range(n)] for i in range(n)]
+        for i in range(n):
+            dp[i][i] = 1
+            if i > 1:
+                dp[i - 1][i] = 2 if s[i] == s[i - 1] else 1
 
-        # j is the length
-        for j in range(1, n + 1):
+        # j is the length of the palindrome
+        for j in range(2, n + 1):
             # i is the starting index
             for i in range(n - j + 1):
-                if j == 1:
-                    dp[i][i + j - 1] = 1
-                    continue
-                if j == 2 and i + 1 < n and s[i] == s[i + 1]:
-                    dp[i][i + 1] = 2
-                    continue
+                dp[i][i + j - 1] = max(dp[i + 1][i + j - 1], dp[i][i + j - 2])
                 if s[i] == s[i + j - 1]:
-                    dp[i][i + j - 1] = dp[i + 1][i + j - 2] + 2
-                else:
-                    dp[i][i + j - 1] = max(dp[i][i + j - 2], dp[i + 1][i + j - 1])
+                    dp[i][i + j - 1] = max(dp[i][i + j - 1], dp[i + 1][i + j - 2] + 2)
 
         return dp[0][-1]
 
