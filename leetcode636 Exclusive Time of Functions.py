56bee7d8fd41301f4d866b3cfce06a9c73a74262
whglamrock <haowu19930908@gmail.com>
2024-03-01 16:09:56 -0800

    add changes

--- leetcode636 Exclusive Time of Functions.py
+++ leetcode636 Exclusive Time of Functions.py
@@ -1,36 +1,28 @@
+from typing import List
 
-class Solution(object):
-    def exclusiveTime(self, n, logs):
-
-        if not logs:
-            return []
-
-        # only saves the f_id
+# we have to use a stack to store the started/ongoing functions instead of using a "executionFunction" single variable
+# to record the start function. Because for every operation you will need to switch context, in which case you will
+# lose track of which function you should execute (i.e., the "executionFunction" may point to the wrong function)
+class Solution:
+    def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         stack = []
-        # each ans[i] is accumulate by adding up all small intervals and gaps between intervals
         ans = [0] * n
-        # record the time stamp of each move (both start and end)
-        prev_time = 0
+        currStartTime = 0
 
         for log in logs:
-            f_id, status, time = log.split(':')
-            f_id, time = int(f_id), int(time)
-            if status == 'start':
-                if stack:   # stack[-1]'s status is always 'start'
-                    ans[stack[-1]] += time - prev_time
-                stack.append(f_id)
-                prev_time = time
+            function, operation, time = log.split(':')
+            function, time = int(function), int(time)
+            if operation == 'start':
+                if stack:
+                    ans[stack[-1]] += time - currStartTime
+                stack.append(function)
+                currStartTime = time
             else:
-                # ans[stack[-1]] won't be changed afterwards
-                ans[stack[-1]] += time + 1 - prev_time
+                ans[stack[-1]] += time - currStartTime + 1
                 stack.pop()
-                prev_time = time + 1
+                currStartTime = time + 1
 
         return ans
 
 
-
-n = 2
-logs = ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]
-Sol = Solution()
-print Sol.exclusiveTime(n, logs)
+print(Solution().exclusiveTime(2, ["0:start:0", "1:start:2", "1:end:5", "0:end:6"]))
