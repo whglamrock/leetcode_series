c4c1f1364467179ef1c22e969385fcd65de76f9e
whglamrock <haowu19930908@gmail.com>
2024-03-03 18:56:55 -0800

    add changes

--- leetcode114 Flatten Binary Tree to Linked List.py
+++ leetcode114 Flatten Binary Tree to Linked List.py
@@ -1,42 +1,34 @@
+from typing import Optional, Tuple
 
-# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-class TreeNode(object):
-    def __init__(self, x):
-
-        self.val = x
-        self.left = None
-        self.right = None
-
-
-# remember to 'node.left = None'!
-
-class Solution(object):
-    def flatten(self, root):
-
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
         if not root:
             return
+        self.flattenTree(root)
 
-        def helper(node):
-            if (not node.left) and (not node.right):
-                return node, node
-            elif (not node.left) and node.right:
-                rhead, rtail = helper(node.right)
-                node.right = rhead
-                node.left = None
-                return node, rtail
-            elif node.left and (not node.right):
-                lhead, ltail = helper(node.left)
-                node.right = lhead
-                node.left = None
-                return node, ltail
-            else:
-                lhead, ltail = helper(node.left)
-                rhead, rtail = helper(node.right)
-                node.right = lhead
-                node.left = None
-                ltail.right = rhead
-                return node, rtail
-                
-        helper(root)
-        #return root  # stupid leetcode asked to return nothing
+    def flattenTree(self, root: Optional[TreeNode]) -> Tuple[TreeNode, TreeNode]:
+        if not root.left and not root.right:
+            return root, root
+        elif root.left and root.right:
+            headOfLeftSubtree, tailOfLeftSubtree = self.flattenTree(root.left)
+            headOfRightSubtree, tailOfRightSubtree = self.flattenTree(root.right)
+            root.left, root.right = None, headOfLeftSubtree
+            tailOfLeftSubtree.right = headOfRightSubtree
+            return root, tailOfRightSubtree
+        elif root.left:
+            headOfLeftSubtree, tailOfLeftSubtree = self.flattenTree(root.left)
+            root.left, root.right = None, headOfLeftSubtree
+            return root, tailOfLeftSubtree
+        else:
+            headOfRightSubtree, tailOfRightSubtree = self.flattenTree(root.right)
+            root.left, root.right = None, headOfRightSubtree
+            return root, tailOfRightSubtree
--- leetcode490 The Maze.py
+++ leetcode490 The Maze.py
@@ -1,39 +1,56 @@
+from typing import List
 
-# clean DFS solution
-#   the BFS version is nothing different but using "pop(0)"
-# P.S., it's very important to know that the
-
-class Solution(object):
-    def hasPath(self, maze, start, destination):
-
-        queue = [start]
+# O(m * n) BFS solution. Note that we don't check visited until we make a turn. This is to deal with an edge case
+# where we would otherwise incorrectly make a turn in a non-near-wall position.
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        leftVisited, rightVisited, upVisited, downVisited = set(), set(), set(), set()
+        directionToVisited = {'up': upVisited, 'down': downVisited, 'left': leftVisited, 'right': rightVisited}
+        todo = [[start[0], start[1], 'up'], [start[0], start[1], 'down'], [start[0], start[1], 'left'], [start[0], start[1], 'right']]
         m, n = len(maze), len(maze[0])
-        # remember this trick
-        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        while queue:
+        while todo:
+            nextTodo = []
+            for i, j, direction in todo:
+                if i == destination[0] and j == destination[1] and self.cantMove(maze, i, j, direction):
+                    return True
+                if (i, j) in directionToVisited[direction]:
+                    continue
+                directionToVisited[direction].add((i, j))
+                # don't check visited here until making a turn
+                if direction == 'up' and (i - 1 >= 0 and maze[i - 1][j] == 0):
+                    nextTodo.append([i - 1, j, 'up'])
+                elif direction == 'down' and (i + 1 < m and maze[i + 1][j] == 0):
+                    nextTodo.append([i + 1, j, 'down'])
+                elif direction == 'left' and (j - 1 >= 0 and maze[i][j - 1] == 0):
+                    nextTodo.append([i, j - 1, 'left'])
+                elif direction == 'right' and (j + 1 < n and maze[i][j + 1] == 0):
+                    nextTodo.append([i, j + 1, 'right'])
+                # can't keep moving in original direction, see if we can turn
+                else:
+                    if (i - 1 >= 0 and maze[i - 1][j] == 0) and (i - 1, j) not in upVisited:
+                        nextTodo.append([i - 1, j, 'up'])
+                    if (i + 1 < m and maze[i + 1][j] == 0) and (i + 1, j) not in downVisited:
+                        nextTodo.append([i + 1, j, 'down'])
+                    if (j - 1 >= 0 and maze[i][j - 1] == 0) and (i, j - 1) not in leftVisited:
+                        nextTodo.append([i, j - 1, 'left'])
+                    if (j + 1 < n and maze[i][j + 1] == 0) and (i, j + 1) not in rightVisited:
+                        nextTodo.append([i, j + 1, 'right'])
 
-            # using "pop(0)" becomes a BFS!
-            i, j = queue.pop()
-            maze[i][j] = 2
-
-            if [i, j] == destination:
-                return True
-
-            for x, y in dirs:
-                row, col = i, j
-
-                # the condition is "!= 1" not "== 0" because
-                #   when it "== 2", we can still go through the stop point that we've been to
-                while 0 <= row < m and 0 <= col < n and maze[row][col] != 1:
-                    row += x
-                    col += y
-
-                # P.S. it is possible that the above while loop didn't even execute
-                row -= x
-                col -= y
-                # so we need this check:
-                if maze[row][col] == 0:
-                    queue.append([row, col])
+            todo = nextTodo
 
         return False
+
+    def cantMove(self, maze: List[List[int]], i: int, j: int, direction: str) -> bool:
+        if direction == 'up':
+            return i - 1 < 0 or maze[i - 1][j] == 1
+        if direction == 'down':
+            return i + 1 >= len(maze) or maze[i + 1][j] == 1
+        if direction == 'left':
+            return j - 1 < 0 or maze[i][j - 1] == 1
+        if direction == 'right':
+            return j + 1 >= len(maze[0]) or maze[i][j + 1] == 1
+        return False
+
+
+print(Solution().hasPath([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0], [1, 2]))
