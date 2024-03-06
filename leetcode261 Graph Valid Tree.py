c20f52dd3675515229ef7b45a67a3778995a30d4
whglamrock <haowu19930908@gmail.com>
2024-03-05 19:41:02 -0800

    add changes

--- leetcode261 Graph Valid Tree.py
+++ leetcode261 Graph Valid Tree.py
@@ -1,39 +1,34 @@
+from collections import defaultdict
+from typing import List
 
-# Typical union find solution. remember the find() and union() methods
-
-class Solution(object):
-    def validTree(self, n, edges):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :rtype: bool
-        """
-        if n == 0:
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # not absolutely necessary but it can speed up the algorithm
+        if len(edges) != n - 1:
             return False
-        if not edges:
-            return n <= 1
 
-        # the path compression will make sure if two nodes are in the same union, the find() will output the same parent
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]
-
-        def union(x, y):
-            # when we union 2 parts(each part contains a bunch of nodes) of the tree, the following line won't
-                # update all parents of x set to find(y);
-            # i.e., it will only compress the path in x set first then do the union
-            parent[find(x)] = find(y)
-
-        parent = range(n)
+        graph = defaultdict(set)
         for i, j in edges:
-            x = find(i)
-            y = find(j)
-            # circle exists
-            if x == y:
-                return False
-            union(i, j)
-            # print parent
+            graph[i].add(j)
+            graph[j].add(i)
 
-        return len(edges) == n - 1
+        todo = {0}
+        visited = set()
+        while todo:
+            nextTodo = set()
+            for node in todo:
+                if node in visited:
+                    return False
+                visited.add(node)
+                for nextNode in graph[node]:
+                    graph[nextNode].discard(node)
+                    if not graph[nextNode]:
+                        del graph[nextNode]
+                    # edge case: a ring of 4 nodes (or any even number of nodes)
+                    if nextNode in nextTodo:
+                        return False
+                    nextTodo.add(nextNode)
+                del graph[node]
+            todo = nextTodo
 
+        return len(visited) == n
