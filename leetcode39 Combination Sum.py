699477fb8e5194a04795ec2f70bb2b6c734093bb
whglamrock <haowu19930908@gmail.com>
2024-03-05 23:52:39 -0800

    add changes

--- leetcode39 Combination Sum.py
+++ leetcode39 Combination Sum.py
@@ -1,62 +1,21 @@
-from collections import defaultdict
-from copy import deepcopy
-from typing import List, Dict
+from typing import List
 
 class Solution:
+    def __init__(self):
+        self.ans = []
+
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        todo = {('', 0)}
-        visited = set()
-        workingNumCountStrs = set()
-        while todo:
-            nextTodo = set()
-            for serializedNumCount, currSum in todo:
-                visited.add(serializedNumCount)
-                if currSum == target:
-                    workingNumCountStrs.add(serializedNumCount)
-                    continue
+        candidates.sort()
+        self.ans = []
+        self.dfs(0, [], 0, candidates, target)
+        return self.ans
 
-                numCount = self.deserializeToNumCount(serializedNumCount)
-                for nextNum in candidates:
-                    if currSum + nextNum > target:
-                        continue
-                    nextNumCount = deepcopy(numCount)
-                    nextNumCount[nextNum] += 1
-                    serializedNexNumCount = self.serializeNumCount(nextNumCount)
-                    if serializedNexNumCount in visited:
-                        continue
-                    visited.add(serializedNexNumCount)
-                    nextSum = currSum + nextNum
-                    nextTodo.add((serializedNexNumCount, nextSum))
+    def dfs(self, index: int, curr: List[int], currSum: int, candidates: List[int], target: int):
+        if currSum > target:
+            return
+        if currSum == target:
+            self.ans.append(curr)
+            return
+        for i in range(index, len(candidates)):
+            self.dfs(i, curr + [candidates[i]], currSum + candidates[i], candidates, target)
 
-            todo = nextTodo
-
-        ans = []
-        for numCountStr in workingNumCountStrs:
-            numCount = self.deserializeToNumCount(numCountStr)
-            curr = []
-            for num in numCount:
-                for i in range(numCount[num]):
-                    curr.append(num)
-            ans.append(curr)
-
-        return ans
-
-    def serializeNumCount(self, numCount: Dict[int, int]) -> str:
-        sortedNums = sorted(numCount.keys())
-        serialized = []
-        for num in sortedNums:
-            serialized.append(str(num) + ':' + str(numCount[num]))
-        return ','.join(serialized)
-
-    def deserializeToNumCount(self, numCountStr: str) -> Dict[int, int]:
-        if not numCountStr:
-            return defaultdict(int)
-
-        tokens = numCountStr.split(',')
-        numCount = defaultdict(int)
-        for token in tokens:
-            splitterIndex = token.index(':')
-            num = int(token[:splitterIndex])
-            count = int(token[splitterIndex + 1:])
-            numCount[num] = count
-        return numCount
--- leetcode503 Next Greater Element II.py
+++ leetcode503 Next Greater Element II.py
@@ -1,24 +1,21 @@
+from typing import List
 
-# the idea is still: find consecutively descending subsequence, and the next number will be
-#   the "next greater element" of all previous elements in the subsequence
-
-class Solution(object):
-    def nextGreaterElements(self, nums):
-
-        if not nums:
-            return []
-
+# decreasing stack + loop twice
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        # stack stores the indices of decreasing elements
+        ans = [-1] * n
         stack = []
-        nextgreater = [-1 for i in xrange(n)]
 
-        for i in xrange(2 * n):
-            num = nums[i % n]
-            while stack and num > nums[stack[-1]]:
-                lasti = stack.pop()
-                nextgreater[lasti] = num
-            if i < n:
-                stack.append(i)
+        for i in range(n):
+            while stack and stack[-1][1] < nums[i]:
+                prevIndex, prevNum = stack.pop()
+                ans[prevIndex] = nums[i]
+            stack.append([i, nums[i]])
+        for i in range(n):
+            while stack and stack[-1][1] < nums[i]:
+                prevIndex, prevNum = stack.pop()
+                ans[prevIndex] = nums[i]
+            stack.append([i, nums[i]])
 
-        return nextgreater
+        return ans
