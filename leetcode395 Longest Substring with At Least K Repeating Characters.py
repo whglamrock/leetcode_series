423444957f870bff029ae10a5905e6380555088e
whglamrock <haowu19930908@gmail.com>
2024-03-12 13:12:46 -0700

    add changes

--- leetcode395 Longest Substring with At Least K Repeating Characters.py
+++ leetcode395 Longest Substring with At Least K Repeating Characters.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from copy import deepcopy
 from typing import Dict
 
-# O(N^2) question that should be acceptable in real interview but got TLE in the stupid leetcode
+# O(N ^ 2) question that should be acceptable in real interview but got TLE in the stupid leetcode
 class Solution:
     def longestSubstring(self, s: str, k: int) -> int:
         charCount = defaultdict(int)
@@ -23,6 +23,7 @@ class Solution:
                 prefixCharCount = indexToCharCount[i]
                 if self.isSubstrCharCountsMoreThanK(prefixCharCount, currCharCount, k):
                     ans = max(ans, j - i)
+                    break
         return ans
 
     # O(1) because s is all lowercase letters
@@ -37,18 +38,19 @@ class Solution:
 
 
 '''
-# a stupid divide and conquer question initially asked by baidu...
-# O(N^2) time
-class Solution(object):
-    def longestSubstring(self, s, k):
+# a stupid divide and conquer question initially asked by baidu. In worst case it's still O(N ^ 2) time.
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
         if k == 1:
             return len(s)
         if len(s) < k:
             return 0
-
-        for c in set(s):    # max length of set is 26.
-            if s.count(c) < k:  # s.count and s.split both take O(N). Recursion takes O(N). So in total O(N^2)
-                return max(self.longestSubstring(substring, k) for substring in s.split(c))
-
+        
+        charCount = Counter(s)
+        for char in charCount:
+            # we don't need to check any other char whose count < k
+            if charCount[char] < k:
+                return max(self.longestSubstring(substr, k) for substr in s.split(char))
+        
         return len(s)
 '''
