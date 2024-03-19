afa017e29f76b02a11a2d48045c6ccfe53608790
whglamrock <haowu19930908@gmail.com>
2024-03-18 18:54:21 -0700

    add changes

--- leetcode226 Invert Binary Tree.py
+++ leetcode226 Invert Binary Tree.py
@@ -1,57 +1,17 @@
+from typing import Optional
 
-# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-class TreeNode(object):
-    def __init__(self, x):
-
-        self.val = x
-        self.left = None
-        self.right = None
-
-
-class Solution(object):
-    def invertTree(self, root):
-        
-        if (not root):
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
             return
-        else:
-            temp1 = root.left
-            temp2 = root.right
-            root.left = temp2
-            root.right = temp1
-            self.invertTree(root.left)
-            self.invertTree(root.right)
-
+        leftChild, rightChild = root.left, root.right
+        self.invertTree(leftChild)
+        self.invertTree(rightChild)
+        root.left, root.right = rightChild, leftChild
         return root
-
-
-
-a = TreeNode(1)
-b = TreeNode(2)
-c = TreeNode(3)
-d = TreeNode(4)
-e = TreeNode(5)
-f = TreeNode(6)
-g = TreeNode(7)
-h = TreeNode(8)
-i = TreeNode(9)
-j = TreeNode(10)
-k = TreeNode(11)
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.left = f
-c.right = g
-d.left = h
-d.right = i
-f.left = j
-g.left = k
-
-Sol = Solution()
-root = Sol.invertTree(a)
-print root.val
-print root.left.val
-print root.left.left.val
-print root.left.left.right.val
-
--- leetcode503 Next Greater Element II.py
+++ leetcode503 Next Greater Element II.py
@@ -1,21 +1,26 @@
+from collections import deque
 from typing import List
 
-# decreasing stack + loop twice
+# double the nums array + decreasing queue from right to left
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        nums = nums + nums
         n = len(nums)
-        ans = [-1] * n
-        stack = []
+        decreasingQueue = deque()
+        for i in range(n - 1, n // 2 - 1, -1):
+            while decreasingQueue and decreasingQueue[0][1] <= nums[i]:
+                decreasingQueue.popleft()
+            decreasingQueue.appendleft([i, nums[i]])
 
-        for i in range(n):
-            while stack and stack[-1][1] < nums[i]:
-                prevIndex, prevNum = stack.pop()
-                ans[prevIndex] = nums[i]
-            stack.append([i, nums[i]])
-        for i in range(n):
-            while stack and stack[-1][1] < nums[i]:
-                prevIndex, prevNum = stack.pop()
-                ans[prevIndex] = nums[i]
-            stack.append([i, nums[i]])
+        ans = [-1] * (n // 2)
+        for i in range(n // 2 - 1, -1, -1):
+            while decreasingQueue and decreasingQueue[0][1] <= nums[i]:
+                decreasingQueue.popleft()
+            if decreasingQueue:
+                ans[i] = decreasingQueue[0][1]
+            decreasingQueue.appendleft([i, nums[i]])
+            # i + n // 2 - 1 corresponds to the next for loop
+            while decreasingQueue and decreasingQueue[-1][0] >= i + n // 2 - 1:
+                decreasingQueue.pop()
 
         return ans
