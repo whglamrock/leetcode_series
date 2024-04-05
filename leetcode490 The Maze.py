01fcb9cd2372ad9666f7d27abe69fe913699f2c5
whglamrock <haowu19930908@gmail.com>
2024-04-04 20:58:34 -0700

    add changes

--- leetcode490 The Maze.py
+++ leetcode490 The Maze.py
@@ -1,58 +1,34 @@
 from typing import List
 
-# O(m * n) BFS solution. Note that we don't check visited until we make a turn. This is to deal with an edge case
-# where we would otherwise incorrectly make a turn in a non-near-wall position.
+# Below solution is easier to implement (way less code) and less error-prone than using visited set for each direction.
+# Also notice that in BFS we don't add the position to the nextTodo unless we can stop.
 class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
-        leftVisited, rightVisited, upVisited, downVisited = set(), set(), set(), set()
-        directionToVisited = {'up': upVisited, 'down': downVisited, 'left': leftVisited, 'right': rightVisited}
-        todo = set()
-        for direction in ['up', 'down', 'left', 'right']:
-            todo.add((start[0], start[1], direction))
         m, n = len(maze), len(maze[0])
+        todo = {(start[0], start[1])}
 
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
         while todo:
             nextTodo = set()
-            for i, j, direction in todo:
-                if (i, j) in directionToVisited[direction]:
-                    continue
-                if i == destination[0] and j == destination[1] and self.cantMove(maze, i, j, direction):
+            for i, j in todo:
+                if i == destination[0] and j == destination[1]:
                     return True
-                directionToVisited[direction].add((i, j))
-                # don't check visited here until making a turn. Let the ball slide into the next position
-                # even if it's visited. It's gonna be skipped in the nextTodo anyways
-                if direction == 'up' and (i - 1 >= 0 and maze[i - 1][j] == 0):
-                    nextTodo.add((i - 1, j, 'up'))
-                elif direction == 'down' and (i + 1 < m and maze[i + 1][j] == 0):
-                    nextTodo.add((i + 1, j, 'down'))
-                elif direction == 'left' and (j - 1 >= 0 and maze[i][j - 1] == 0):
-                    nextTodo.add((i, j - 1, 'left'))
-                elif direction == 'right' and (j + 1 < n and maze[i][j + 1] == 0):
-                    nextTodo.add((i, j + 1, 'right'))
-                # hit a wall; see if we can turn
-                else:
-                    if i - 1 >= 0 and maze[i - 1][j] == 0 and (i - 1, j) not in upVisited:
-                        nextTodo.add((i - 1, j, 'up'))
-                    if i + 1 < m and maze[i + 1][j] == 0 and (i + 1, j) not in downVisited:
-                        nextTodo.add((i + 1, j, 'down'))
-                    if j - 1 >= 0 and maze[i][j - 1] == 0 and (i, j - 1) not in leftVisited:
-                        nextTodo.add((i, j - 1, 'left'))
-                    if j + 1 < n and maze[i][j + 1] == 0 and (i, j + 1) not in rightVisited:
-                        nextTodo.add((i, j + 1, 'right'))
+                # change it to 2 so we shouldn't stop at this position again
+                # but not change it to 1 because we can still move through it
+                maze[i][j] = 2
+                for deltaI, deltaJ in directions:
+                    # remember the below setup for the maze problem (stop until hitting the wall)
+                    ii, jj = i + deltaI, j + deltaJ
+                    while 0 <= ii < m and 0 <= jj < n and maze[ii][jj] != 1:
+                        ii += deltaI
+                        jj += deltaJ
+                    ii -= deltaI
+                    jj -= deltaJ
+                    if maze[ii][jj] == 0:
+                        nextTodo.add((ii, jj))
 
             todo = nextTodo
 
-        return False
-
-    def cantMove(self, maze: List[List[int]], i: int, j: int, direction: str) -> bool:
-        if direction == 'up':
-            return i - 1 < 0 or maze[i - 1][j] == 1
-        if direction == 'down':
-            return i + 1 >= len(maze) or maze[i + 1][j] == 1
-        if direction == 'left':
-            return j - 1 < 0 or maze[i][j - 1] == 1
-        if direction == 'right':
-            return j + 1 >= len(maze[0]) or maze[i][j + 1] == 1
         return False
 
 
--- leetcode505 The Maze II.py
+++ leetcode505 The Maze II.py
@@ -1,53 +1,32 @@
-
 from heapq import *
+from typing import List
 
-# Using heapq won't reduce the mathematical time complexity, but can make sure we don't make redundant BFS
-# Time complexity should be O(M * N)
-
-class Solution(object):
-    def shortestDistance(self, maze, start, destination):
-        """
-        :type maze: List[List[int]]
-        :type start: List[int]
-        :type destination: List[int]
-        :rtype: int
-        """
-
-        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        start, destination = tuple(start), tuple(destination)
-        # pq is the min distance pq
-        pq = [(0, start)]
-        heapify(pq)
-
-        ans = 2147483647
-        visited = set()
-
+class Solution:
+    def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
+        m, n = len(maze), len(maze[0])
+        pq = [[0, start[0], start[1]]]
+        indexToMinDist = {}
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
         while pq:
-            distance, position = heappop(pq)
-            # because we visited the same position with min distance first, we don't change the ans here
-            if position in visited:
+            distance, i, j = heappop(pq)
+            if i == destination[0] and j == destination[1]:
+                return distance
+            if (i, j) in indexToMinDist and indexToMinDist[(i, j)] < distance:
                 continue
-            visited.add(position)
+            indexToMinDist[(i, j)] = distance
 
-            if position == destination:
-                ans = distance
-                break
+            for deltaI, deltaJ in directions:
+                ii, jj = i + deltaI, j + deltaJ
+                nextDistance = distance
+                while 0 <= ii < m and 0 <= jj < n and maze[ii][jj] == 0:
+                    ii += deltaI
+                    jj += deltaJ
+                    nextDistance += 1
+                ii -= deltaI
+                jj -= deltaJ
+                if (ii, jj) in indexToMinDist and indexToMinDist[(ii, jj)] <= nextDistance:
+                    continue
+                indexToMinDist[(ii, jj)] = nextDistance
+                heappush(pq, [nextDistance, ii, jj])
 
-            # we have to do BFS here, not DFS
-            for direction in dirs:
-                newDist, newPos = self.go(position, maze, direction, distance)
-                if newPos not in visited:
-                    heappush(pq, (newDist, newPos))
-
-        return ans if ans != 2147483647 else -1
-
-    def go(self, curr, maze, direction, distance):
-        i, j = curr
-        x, y = direction
-
-        while 0 <= i + x < len(maze) and 0 <= j + y < len(maze[0]) and maze[i + x][j + y] != 1:
-            i += x
-            j += y
-            distance += 1
-
-        return distance, (i, j)
+        return -1
