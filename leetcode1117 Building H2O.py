dc32210d93b322de51b1e4ae124e03f48c2bf2a1
whglamrock <haowu19930908@gmail.com>
2024-05-30 10:10:04 -0700

    add changes

--- leetcode1117 Building H2O.py
+++ leetcode1117 Building H2O.py
@@ -2,18 +2,17 @@ from threading import Semaphore, Barrier
 
 class H2O:
     def __init__(self):
-        self.semH = Semaphore(2)
-        self.semO = Semaphore(1)
+        self.semH, self.semO = Semaphore(2), Semaphore(1)
         self.bar = Barrier(3)
 
     def hydrogen(self, releaseHydrogen: 'Callable[[], None]') -> None:
         with self.semH:
-            self.bar.wait()
             # releaseHydrogen() outputs "H". Do not change or remove this line.
             releaseHydrogen()
+            self.bar.wait()
 
     def oxygen(self, releaseOxygen: 'Callable[[], None]') -> None:
         with self.semO:
-            self.bar.wait()
             # releaseOxygen() outputs "O". Do not change or remove this line.
             releaseOxygen()
+            self.bar.wait()
