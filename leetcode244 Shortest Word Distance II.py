a103e5f05e0ae6ee29a7cc9fb2e66ec9e160c70a
whglamrock <haowu19930908@gmail.com>
2024-08-01 15:12:24 -0700

    add changes

--- leetcode244 Shortest Word Distance II.py
+++ leetcode244 Shortest Word Distance II.py
@@ -9,54 +9,51 @@ class WordDistance:
             self.wordToIndexes[word].append(i)
 
     def shortest(self, word1: str, word2: str) -> int:
-        indexes1, indexes2 = self.wordToIndexes[word1], self.wordToIndexes[word2]
-        if len(indexes1) > len(indexes2):
-            indexes1, indexes2 = indexes2, indexes1
+        # always make word2 has more indexes
+        if len(self.wordToIndexes[word1]) > len(self.wordToIndexes[word2]):
+            word1, word2 = word2, word1
 
-        shortestDist = 2147483647
-        for target in indexes1:
-            closestIndex = self.findClosest(indexes2, target)
-            shortestDist = min(shortestDist, abs(closestIndex - target))
+        indexesOfWord2 = self.wordToIndexes[word2]
+        minDistance = 2147483647
+        for index1 in self.wordToIndexes[word1]:
+            closestSmaller = self.findClosestSmaller(indexesOfWord2, index1)
+            closestBigger = self.findClosestBigger(indexesOfWord2, index1)
+            if closestSmaller != -1:
+                minDistance = min(minDistance, index1 - closestSmaller)
+            if closestBigger != -1:
+                minDistance = min(minDistance, closestBigger - index1)
 
-        return shortestDist
+        return minDistance
 
-    def findClosest(self, nums: List[int], target: int) -> int:
-        closestSmaller, closestBigger = -1, -1
-
-        # go smaller
+    def findClosestSmaller(self, nums: List[int], target: int):
         l, r = 0, len(nums) - 1
         while l <= r:
             m = (l + r + 1) // 2
             if l == r:
-                if nums[m] <= target:
-                    closestSmaller = nums[m]
-                break
-            if nums[m] <= target:
+                if nums[m] < target:
+                    return nums[m]
+                return -1
+            if nums[m] < target:
                 l = m
             else:
                 r = m - 1
 
-        # go bigger
+        return -1
+
+    def findClosestBigger(self, nums: List[int], target: int):
         l, r = 0, len(nums) - 1
         while l <= r:
             m = (l + r) // 2
             if l == r:
-                if nums[m] >= target:
-                    closestBigger = nums[m]
-                break
-            if nums[m] >= target:
+                if nums[m] > target:
+                    return nums[m]
+                return -1
+            if nums[m] > target:
                 r = m
             else:
                 l = m + 1
 
-        if closestSmaller == -1:
-            return closestBigger
-        if closestBigger == -1:
-            return closestSmaller
-        if target - closestSmaller <= closestBigger - target:
-            return closestSmaller
-        else:
-            return closestBigger
+        return -1
 
 
 # Your WordDistance object will be instantiated and called as such:
